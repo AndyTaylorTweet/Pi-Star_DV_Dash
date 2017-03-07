0fd6eef504d1a207e88ee5cb90beb8cb190f47ea
Andy Taylor <andy@always-online.co.uk>
2017-03-07 15:04:21 +0000

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -24,7 +24,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/update.php" || $_SERVER["PHP_SELF"] == "/adm
   else {
     system('sudo nohup rm -rf /var/log/pi-star/pi-star_update.log');
     system('sudo touch /var/log/pi-star/pi-star_update.log');
-    system('sudo /usr/local/sbin/pistar-update &');
+    system('sudo nohup /usr/local/sbin/pistar-update &');
     }
  unset($_SESSION['offset']);
   
