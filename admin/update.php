8765f2c47453ba614a393a862a4d9a2af0f6ba63
Andy Taylor <andy@always-online.co.uk>
2017-03-07 15:25:02 +0000

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -69,7 +69,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/update.php") {
   <div id="contentwide">
   <table width="100%">
   <tr><th>Update Running</th></tr>
-  <tr><td align="left"><div id="tail">Starting update...<br></div></td></tr>
+  <tr><td align="left"><div id="tail">Starting update, please wait...<br></div></td></tr>
   </table>
   </div>
   <div id="footer">
