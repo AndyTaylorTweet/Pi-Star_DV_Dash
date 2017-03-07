2e037be4472357cbdef3a06c97aabf0dc79c9a2f
Andy Taylor <andy@always-online.co.uk>
2017-03-07 13:13:51 +0000

    Update update.php

--- admin/update.php
+++ admin/update.php
@@ -8,6 +8,10 @@ if ($_SERVER["PHP_SELF"] == "/admin/update.php") {
   //Load the pistar-release file
   $pistarReleaseConfig = '/etc/pistar-release';
   $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
+
+  header('Content-Type: text/event-stream');
+  header('Cache-Control: no-cache');
+  
 ?>
   <!doctype html>
   <html xmlns="http://www.w3.org/1999/xhtml"xmlns:v="urn:schemas-microsoft-com:vml">
