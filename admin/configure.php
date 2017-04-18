cf6f7e359f6a7d479c66c10af141b920a55f4834
Andy Taylor <andy@always-online.co.uk>
2017-04-18 20:18:51 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -286,9 +286,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	if (empty($_POST['selectedAPRSHost']) != TRUE ) {
 	  $rollAPRSHost = 'sudo sed -i "/aprsHostname=/c\\aprsHostname='.escapeshellcmd($_POST['selectedAPRSHost']).'" /etc/ircddbgateway';
 	  system($rollAPRSHost);
-	  $rollAPRSHostYSF = 'sudo sed -i "/Server=/c\\Server='.escapeshellcmd($_POST['selectedAPRSHost']).'" /etc/ysfgateway';
-	  system($rollAPRSHostYSF);
-
+	  $configysfgateway['aprs.fi']['Server'] = escapeshellcmd($_POST['selectedAPRSHost']);
 	  }
 
 	// Set the Frequency for Duplex
