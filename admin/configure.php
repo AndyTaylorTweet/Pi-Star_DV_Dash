34d7b9952e7b60ea40c679b4fe8477e8036781e3
Andy Taylor <andy@always-online.co.uk>
2017-04-19 16:59:16 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -337,7 +337,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	  $rollBEACONTEXT = 'sudo sed -i "/beaconText=/c\\beaconText='.$confRPT1.'" /etc/dstarrepeater';
 	  $rollDVVARIANT = 'sudo sed -i "/dvmegaVariant=/c\\dvmegaVariant='.$confDVVariant.'" /etc/dstarrepeater';
 	  $rollIRCrepeaterBand1 = 'sudo sed -i "/repeaterBand1=/c\\repeaterBand1='.$confIRCrepeaterBand1.'" /etc/ircddbgateway';
-	  $rollIRCrepeaterCall1 = 'sudo sed -i "/repeaterCall1=/c\\repeaterCall1='.$newCallsignUpper.'" /etc/ircddbgateway';
+	  $rollIRCrepeaterCall1 = 'sudo sed -i "/repeaterCall1=/c\\repeaterCall1='.strtoupper(escapeshellcmd($_POST['confCallsign'])).'" /etc/ircddbgateway';
 
 	  system($rollRPT1);
 	  system($rollRPT2);
