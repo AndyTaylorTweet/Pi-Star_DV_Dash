c782473ff933b0687149fee832ff3d2b401b8375
Andy Taylor <andy@always-online.co.uk>
2017-05-21 17:32:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,10 @@ $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
 <p style="padding-right: 5px; text-align: right; color: #ffffff;">
  <a href="/" alt="Dashboard" style="color: #ffffff;">Dashboard</a> |
  <a href="/admin/" alt="Administration" style="color: #ffffff;">Admin</a> |
- <a href="/admin/power.php" alt="Power Control" style="color: #ffffff;">Power</a> |
+<?php if ($_SERVER["PHP_SELF"] == "/admin/index.php") {
+  echo ' <a href="/admin/power.php" alt="Power Control" style="color: #ffffff;">Power</a> |'."\n";
+  echo ' <a href="/admin/update.php" alt="Update" style="color: #ffffff;">Update</a> |'."\n";
+  } ?>
  <a href="/admin/configure.php" alt="Configuration" style="color: #ffffff;">Config</a>
 </p>
 </div>
