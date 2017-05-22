7486700d0ee76ed0c5e947bfe7782d5b622ac7d5
Andy Taylor <andy@always-online.co.uk>
2017-05-22 21:44:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -201,6 +201,7 @@ Pi-Star / Pi-Star Dashboard, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y
 ircDDBGateway Dashboard by Hans-J. Barthen (DL5DI),<br />
 MMDVMDash developed by Kim Huebel (DG9VH),<br />
 Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.mw0mwz.co.uk/pi-star/" target="_new">here</a>.<br />
+Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistar/" target="_new">here for the Support Group</a>
 </div>
 
 </div>
