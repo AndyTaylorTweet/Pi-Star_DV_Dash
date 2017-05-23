1ce293aca9e1dfe7651665e0bd6647e242851733
Andy Taylor <andy@always-online.co.uk>
2017-05-24 00:22:07 +0100

    Update config_backup.php

--- admin/config_backup.php
+++ admin/config_backup.php
@@ -95,8 +95,48 @@ if ($_SERVER["PHP_SELF"] == "/admin/config_backup.php") {
 
           $target_dir = "/tmp/config_restore";
           $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+          $uploadOk = 1;
           $output .= shell_exec("sudo rm -rf $target_dir 2>&1");
           $output .= shell_exec("sudo mkdir $target_dir 2>&1");
+          
+          $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+          // Check if image file is a actual image or fake image
+          if(isset($_POST["submit"])) {
+              $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+              if($check !== false) {
+                  $output .= "File is an image - " . $check["mime"] . ".";
+                  $uploadOk = 1;
+              } else {
+                  $output .= "File is not an image.";
+                  $uploadOk = 0;
+              }
+          }
+          // Check if file already exists
+          if (file_exists($target_file)) {
+              $output .= "Sorry, file already exists.";
+              $uploadOk = 0;
+          }
+          // Check file size
+          if ($_FILES["fileToUpload"]["size"] > 10000) {
+              $output .= "Sorry, your file is too large.";
+              $uploadOk = 0;
+          }
+          // Allow certain file formats
+          if($imageFileType != "zip") {
+              $output .= "Sorry, only ZIP files are allowed.";
+              $uploadOk = 0;
+          }
+          // Check if $uploadOk is set to 0 by an error
+          if ($uploadOk == 0) {
+              $output .= "Sorry, your file was not uploaded.";
+          // if everything is ok, try to upload file
+          } else {
+              if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                  $output .= "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+              } else {
+                  $output .= "Sorry, there was an error uploading your file.";
+              }
+          }
           
           echo "<tr><td align=\"left\"><pre>$output</pre></td></tr>\n";
             
