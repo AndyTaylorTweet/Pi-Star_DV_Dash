6e1a96d8da4d61beed680c13fd21ebc77427f299
Andy Taylor <andy@always-online.co.uk>
2017-05-26 15:56:05 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -993,8 +993,8 @@ else:
     <td align="left"><a class=tooltip2 href="#">Radio/Modem Type:<span><b>Radio/Modem</b>What kind of radio or modem<br />hardware do you have ?</span></a></td>
     <td align="left" colspan="2"><select name="confHardware">
                 <option<?php if ($configdstar['dvmegaPort'] === '/dev/ttyAMA0') { echo ' selected';}?> value="DVM-RPI">DV-Mega RPi Radio</option>
-                <option<?php if ($configdstar['dvmegaPort'] === '/dev/ttyACM0' && $configdstar[dvmegaVariant] >= 1 ) { echo ' selected';}?> value="DVM-USB">Zum Board / DV-Mega USB Radio</option>
-                <option<?php if ($configdstar['dvmegaVariant'] === '0') { echo ' selected';}?> value="DVM-GMSK">Blue-DV / Bluestack / DV-Mega USB GMSK Modem</option>
+                <option<?php if ($configdstar['dvmegaPort'] === '/dev/ttyACM0' && $configdstar[dvmegaVariant] >= 1 ) { echo ' selected';}?> value="DVM-USB">Zum Board / DV-Mega USB Radio / DV-Mega USB GMSK Node (Old Firmware)</option>
+                <option<?php if ($configdstar['dvmegaVariant'] === '0') { echo ' selected';}?> value="DVM-GMSK">Blue-DV / Bluestack / DV-Mega USB GMSK Node (New Firmware)</option>
                 <option<?php if ($configdstar['modemType'] === 'DV-RPTR V1') { echo ' selected';}?> value="DV-RPTR1">DV-RPTR V1</option>
                 <option<?php if ($configdstar['modemType'] === 'DV-RPTR V2') { echo ' selected';}?> value="DV-RPTR2">DV-RPTR V2</option>
                 <option<?php if ($configdstar['modemType'] === 'DV-RPTR V3') { echo ' selected';}?> value="DV-RPTR3">DV-RPTR V3</option>
