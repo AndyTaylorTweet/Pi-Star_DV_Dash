07bf61a31cead0ddc6abc8564fb362891e82893d
Andy Taylor <andy@always-online.co.uk>
2017-05-27 08:03:50 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -56,13 +56,14 @@ $progname = basename($_SERVER['SCRIPT_FILENAME'],".php");
 $rev=$version;
 $MYCALL=strtoupper($callsign);
 ?>
-<!doctype html>
-<html xmlns="http://www.w3.org/1999/xhtml"xmlns:v="urn:schemas-microsoft-com:vml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
 <head>
     <meta name="robots" content="index" />
     <meta name="robots" content="follow" />
     <meta name="language" content="English" />
-    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <?php echo "<meta name=\"GENERATOR\" content=\"$progname $rev\" />\n"; ?>
     <meta name="Author" content="Andrew Taylor (MW0MWZ)" />
     <meta name="Description" content="Pi-Star Configuration" />
