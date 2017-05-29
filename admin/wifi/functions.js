eccc5a2995e7815d8fec0daff8125ef1a5f766bf
Andy Taylor <andy@always-online.co.uk>
2017-05-29 17:01:17 +0100

    Update functions.js

--- admin/wifi/functions.js
+++ admin/wifi/functions.js
@@ -27,7 +27,7 @@ function AddScanned(network) {
 	if(existing != 0) {
 	        Networks++;
 	}
-        document.getElementById('Networks').value=Networks;
+        //document.getElementById('Networks').value=Networks;
         document.getElementById('networkbox').innerHTML += '<div id="Networkbox'+Networks+'" class="Networkboxes">Network '+Networks+'<input type="button" value="Delete" /><br /> \
 <span class="tableft">SSID :</span><input type="text" name="ssid'+Networks+'" id="ssid'+Networks+'" onkeyup="CheckSSID(this)"><br /> \
 <span class="tableft">PSK :</span><input type="password" name="psk'+Networks+'" onkeyup="CheckPSK(this)"></div>';
