f45047694a3d098e36962ce834ca97cbc3a1b7f5
Andy Taylor <andy@always-online.co.uk>
2017-05-29 17:30:56 +0100

    Update wifi.php

--- admin/wifi.php
+++ admin/wifi.php
@@ -96,7 +96,7 @@ switch($page) {
 		}
 		elseif(isset($_POST['reset_wlan0'])) {
 			exec('sudo wpa_cli reconfigure wlan0 && sudo ifdown wlan0 && sleep 3 && sudo ifup wlan0 && sudo wpa_cli scan',$test);
-			echo 'WiFi Interface reset';
+			echo '<script>window.location.href=\'wifi.php?page=wlan0_info\';</script>';
 		}
 		
 	echo '<div class="infobox">
