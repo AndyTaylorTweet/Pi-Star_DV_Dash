f1c992854035fe55fed65e7c0f56630dbbe4a27a
Andy Taylor <andy@always-online.co.uk>
2017-06-06 23:25:08 +0100

    Update system.php

--- dstarrepeater/system.php
+++ dstarrepeater/system.php
@@ -17,12 +17,12 @@ $MYCALL=strtoupper($callsign);
 <?php
 $cpuLoad = sys_getloadavg();
 $cpuTemp = exec('awk \'{printf("%.1f\n",$1/1e3)}\' /sys/class/thermal/thermal_zone0/temp');
-if ($cpuTemp < 50) { $cpuTempHTML = "<td bgcolor=\"#1d1\">".$cpuTemp."&deg;C</td>\n"; }
-if ($cpuTemp >= 50) { $cpuTempHTML = "<td bgcolor=\"#fa0\">".$cpuTemp."&deg;C</td>\n"; }
-if ($cpuTemp >= 69) { $cpuTempHTML = "<td bgcolor=\"#f00\">".$cpuTemp."&deg;C</td>\n"; }
+if ($cpuTemp < 50) { $cpuTempHTML = "<td style=\"background: #1d1\">".$cpuTemp."&deg;C</td>\n"; }
+if ($cpuTemp >= 50) { $cpuTempHTML = "<td style=\"background: #fa0\">".$cpuTemp."&deg;C</td>\n"; }
+if ($cpuTemp >= 69) { $cpuTempHTML = "<td style=\"background: #f00\">".$cpuTemp."&deg;C</td>\n"; }
 ?>
 <b>Gateway Hardware Information</b>
-<table>
+<table style="table-layout: fixed;">
   <tr>
     <th><a class="tooltip" href="#">Hostname<span><b>Hostname</b></span></a></th>
     <th><a class="tooltip" href="#">Kernel<span><b>Release</b></span></a></th>
@@ -41,12 +41,12 @@ if ($cpuTemp >= 69) { $cpuTempHTML = "<td bgcolor=\"#f00\">".$cpuTemp."&deg;C</t
     <th colspan="6">Service Status</th>
   </tr>
   <tr>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep dstarrepeaterd", $dstarrepeaterpid); if (!empty($dstarrepeaterpid)) { echo "1d1"; } else { echo "b55"; } ?>">DStarRepeater</td>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep MMDVMHost", $mmdvmhostpid); if (!empty($mmdvmhostpid)) { echo "1d1"; } else { echo "b55"; } ?>">MMDVMHost</td>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep ircddbgatewayd", $ircddbgatewaypid); if (!empty($ircddbgatewaypid)) { echo "1d1"; } else { echo "b55"; } ?>">ircDDBGateway</td>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep timeserverd", $timeserverpid); if (!empty($timeserverpid)) { echo "1d1"; } else { echo "b55"; } ?>">TimeServer</td>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep -f -a /usr/local/sbin/pistar-watchdog | sed '/pgrep/d'", $watchdogpid); if (!empty($watchdogpid)) { echo "1d1"; } else { echo "b55"; } ?>">PiStar-Watchdog</td>
-    <td width="16.66%" bgcolor="#<?php exec ("pgrep -f -a /usr/local/sbin/pistar-keeper | sed '/pgrep/d'", $keeperpid); if (!empty($keeperpid)) { echo "1d1"; } else { echo "b55"; } ?>">PiStar-Keeper</td>
+    <td style="background: #<?php exec ("pgrep dstarrepeaterd", $dstarrepeaterpid); if (!empty($dstarrepeaterpid)) { echo "1d1"; } else { echo "b55"; } ?>">DStarRepeater</td>
+    <td style="background: #<?php exec ("pgrep MMDVMHost", $mmdvmhostpid); if (!empty($mmdvmhostpid)) { echo "1d1"; } else { echo "b55"; } ?>">MMDVMHost</td>
+    <td style="background: #<?php exec ("pgrep ircddbgatewayd", $ircddbgatewaypid); if (!empty($ircddbgatewaypid)) { echo "1d1"; } else { echo "b55"; } ?>">ircDDBGateway</td>
+    <td style="background: #<?php exec ("pgrep timeserverd", $timeserverpid); if (!empty($timeserverpid)) { echo "1d1"; } else { echo "b55"; } ?>">TimeServer</td>
+    <td style="background: #<?php exec ("pgrep -f -a /usr/local/sbin/pistar-watchdog | sed '/pgrep/d'", $watchdogpid); if (!empty($watchdogpid)) { echo "1d1"; } else { echo "b55"; } ?>">PiStar-Watchdog</td>
+    <td style="background: #<?php exec ("pgrep -f -a /usr/local/sbin/pistar-keeper | sed '/pgrep/d'", $keeperpid); if (!empty($keeperpid)) { echo "1d1"; } else { echo "b55"; } ?>">PiStar-Keeper</td>
   </tr>
 </table>
 <br />
