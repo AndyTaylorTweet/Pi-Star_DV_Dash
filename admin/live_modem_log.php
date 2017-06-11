9aadf29a4da3b1f17ffad162614b04ffd2b02341
Andy Taylor <andy@always-online.co.uk>
2017-06-11 13:13:54 +0100

    Update live_modem_log.php

--- admin/live_modem_log.php
+++ admin/live_modem_log.php
@@ -53,7 +53,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/live_modem_log.php") {
     <meta http-equiv="Pragma" content="no-cache" />
     <meta http-equiv="Expires" content="0" />
     <title>Pi-Star Live Log Viewer</title>
-    <link rel="stylesheet" type="text/css" href="css/ircddb.css" />
+    <link rel="stylesheet" type="text/css" href="css/ircddb.css?version=1.2" />
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
     <script type="text/javascript" src="http://creativecouple.github.com/jquery-timing/jquery-timing.min.js"></script>
     <script type="text/javascript">
