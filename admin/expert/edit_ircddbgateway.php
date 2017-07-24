771e6008f38e0e267947c42caaa7b8639e036b22
Andy Taylor <andy@always-online.co.uk>
2017-07-24 14:16:43 +0100

    Update edit_ircddbgateway.php

--- admin/expert/edit_ircddbgateway.php
+++ admin/expert/edit_ircddbgateway.php
@@ -1,4 +1,44 @@
 <?php
+//Load the Pi-Star Release file
+$pistarReleaseConfig = '/etc/pistar-release';
+$configPistarRelease = array();
+$configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
+//Load the Version Info
+require_once('../config/version.php');
+?>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
+  <head>
+    <meta name="robots" content="index" />
+    <meta name="robots" content="follow" />
+    <meta name="language" content="English" />
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="Author" content="Andrew Taylor (MW0MWZ)" />
+    <meta name="Description" content="Pi-Star Power" />
+    <meta name="KeyWords" content="Pi-Star" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <title>Pi-Star - Digital Voice Dashboard - Expert Editor</title>
+    <link rel="stylesheet" type="text/css" href="../css/ircddb.css?version=1.3" />
+  </head>
+  <body>
+  <div class="container">
+  <div class="header">
+  <div style="font-size: 8px; text-align: right; padding-right: 8px;">Pi-Star:<?php echo $configPistarRelease['Pi-Star']['Version']?> / Dashboard:<?php echo $version; ?></div>
+  <h1>Pi-Star Digital Voice - Power Control</h1>
+  <p style="padding-right: 5px; text-align: right; color: #ffffff;">
+    <a href="/" style="color: #ffffff;">Dashboard</a> |
+    <a href="/admin/" style="color: #ffffff;">Admin</a> |
+    <a href="/admin/update.php" style="color: #ffffff;">Update</a> |
+    <a href="/admin/config_backup.php" style="color: #ffffff;">Backup/Restore</a> |
+    <a href="/admin/configure.php" style="color: #ffffff;">Config</a>
+  </p>
+  </div>
+  <div class="contentwide">
+
+<?php
 // Do some file wrangling...
 exec('sudo cp /etc/ircddbgateway /tmp/aXJjZGRiZ2F0ZXdheQ.tmp');
 exec('sudo chown www-data:www-data /tmp/aXJjZGRiZ2F0ZXdheQ.tmp');
@@ -69,17 +109,31 @@ $parsed_ini = parse_ini_file($filepath, true);
 
 echo '<form action="" method="post">'."\n";
 	foreach($parsed_ini as $section=>$values) {
-		echo "<h3>$section</h3>\n";
 		// keep the section as hidden text so we can update once the form submitted
-		echo "<input type='hidden' value='$section' name='$section' />\n";
+		echo "<input type=\"hidden\" value=\"$section\" name=\"$section\" />\n";
+		echo "<table>\n";
+		echo "<tr><th colspan=\"2\">$section</th></tr>\n";
 		// print all other values as input fields, so can edit. 
 		// note the name='' attribute it has both section and key
 		foreach($values as $key=>$value) {
-			echo "$key: <input type='text' name='{$section}[$key]' value='$value' /><br />\n";
+			echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><input type=\"text\" name=\"{$section}[$key]\" value=\"$value\" /></td></tr>\n";
 		}
+		echo "</table>\n";
 		echo '<input type="submit" value="Save Changes" />'."\n";
 		echo "<br />\n";
 	}
-
-echo "</form>\n</body>\n</html>\n";
+echo "</form>";
 ?>
+</div>
+
+<div class="footer">
+Pi-Star / Pi-Star Dashboard, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
+ircDDBGateway Dashboard by Hans-J. Barthen (DL5DI),<br />
+MMDVMDash developed by Kim Huebel (DG9VH), <br />
+Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistar/" target="_new">here for the Support Group</a><br />
+Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.mw0mwz.co.uk/pi-star/" target="_new">here</a>.<br />
+</div>
+
+</div>
+</body>
+</html>
