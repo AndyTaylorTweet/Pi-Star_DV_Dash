dc404fd87626b484fe32e0383590e7877069a838
Andy Taylor <andy@always-online.co.uk>
2017-07-24 13:52:42 +0100

    Update edit_dstarrepeater.php

--- admin/expert/edit_dstarrepeater.php
+++ admin/expert/edit_dstarrepeater.php
@@ -1,52 +1,42 @@
 <?php
-//require_once('config/version.php');
-
 //Load the Pi-Star Release file
 $pistarReleaseConfig = '/etc/pistar-release';
 $configPistarRelease = array();
 $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
-
+//Load the Version Info
+require_once('config/version.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
-<head>
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" lang="en">
+  <head>
     <meta name="robots" content="index" />
     <meta name="robots" content="follow" />
     <meta name="language" content="English" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <?php echo "<meta name=\"generator\" content=\"$progname $rev\" />\n"; ?>
-    <meta name="Author" content="Hans-J. Barthen (DL5DI), Kim Huebel (DV9VH) and Andy Taylor (MW0MWZ)" />
-    <meta name="Description" content="Pi-Star Dashboard" />
-    <meta name="KeyWords" content="MW0MWZ,MMDVMHost,ircDDBGateway,D-Star,ircDDB,Pi-Star,Blackwood,Wales,DL5DI,DG9VH" />
-    <meta http-equiv="cache-control" content="max-age=0" />
-    <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
-    <meta http-equiv="expires" content="0" />
-    <meta http-equiv="pragma" content="no-cache" />
-    <title><?php echo "$MYCALL" ?> - Digital Voice Dashboard</title>
-<?php include_once "config/browserdetect.php"; ?>
-    <script type="text/javascript" src="/jquery.min.js"></script>
-    <script type="text/javascript" src="/functions.js"></script>
-    <script type="text/javascript">
-      $.ajaxSetup({ cache: false });
-    </script>
-</head>
-<body>
-<div class="container">
-<div class="header">
-<div style="font-size: 8px; text-align: right; padding-right: 8px;">Pi-Star:<?php echo $configPistarRelease['Pi-Star']['Version']?> / Dashboard:<?php echo $version; ?></div>
-<h1>Pi-Star Digital Voice Dashboard for <?php echo $MYCALL; ?></h1>
-<p style="padding-right: 5px; text-align: right; color: #ffffff;">
- <a href="/" style="color: #ffffff;">Dashboard</a> |
- <a href="/admin/" style="color: #ffffff;">Admin</a> |
-<?php if ($_SERVER["PHP_SELF"] == "/admin/index.php") {
-  echo ' <a href="/admin/live_modem_log.php" style="color: #ffffff;">Live Logs</a> |'."\n";
-  echo ' <a href="/admin/power.php" style="color: #ffffff;">Power</a> |'."\n";
-  echo ' <a href="/admin/update.php" style="color: #ffffff;">Update</a> |'."\n";
-  } ?>
- <a href="/admin/configure.php" style="color: #ffffff;">Config</a>
-</p>
-</div>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="Author" content="Andrew Taylor (MW0MWZ)" />
+    <meta name="Description" content="Pi-Star Power" />
+    <meta name="KeyWords" content="Pi-Star" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <title>Pi-Star - Digital Voice Dashboard - Expert Editor</title>
+    <link rel="stylesheet" type="text/css" href="css/ircddb.css?version=1.3" />
+  </head>
+  <body>
+  <div class="container">
+  <div class="header">
+  <div style="font-size: 8px; text-align: right; padding-right: 8px;">Pi-Star:<?php echo $configPistarRelease['Pi-Star']['Version']?> / Dashboard:<?php echo $version; ?></div>
+  <h1>Pi-Star Digital Voice - Power Control</h1>
+  <p style="padding-right: 5px; text-align: right; color: #ffffff;">
+    <a href="/" style="color: #ffffff;">Dashboard</a> |
+    <a href="/admin/" style="color: #ffffff;">Admin</a> |
+    <a href="/admin/update.php" style="color: #ffffff;">Update</a> |
+    <a href="/admin/config_backup.php" style="color: #ffffff;">Backup/Restore</a> |
+    <a href="/admin/configure.php" style="color: #ffffff;">Config</a>
+  </p>
+  </div>
+  <div class="contentwide">
 
 <?php
 // Do some file wrangling...
