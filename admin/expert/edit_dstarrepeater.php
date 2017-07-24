4c7da5f03895bc86ce64c697fa6862ab34397df8
Andy Taylor <andy@always-online.co.uk>
2017-07-24 13:53:44 +0100

    Update edit_dstarrepeater.php

--- admin/expert/edit_dstarrepeater.php
+++ admin/expert/edit_dstarrepeater.php
@@ -4,7 +4,7 @@ $pistarReleaseConfig = '/etc/pistar-release';
 $configPistarRelease = array();
 $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
 //Load the Version Info
-require_once('config/version.php');
+require_once('../config/version.php');
 ?>
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
