e36425c543f7387dce6c42fdf1431246efdf775f
Andy Taylor <andy@always-online.co.uk>
2017-08-01 00:35:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $configPistarRelease = parse_ini_file($pistarReleaseConfig, true);
 <body>
 <div class="container">
 <div class="header">
-<div style="font-size: 8px; text-align: right; padding-right: 8px;">Pi-Star:<?php echo $configPistarRelease['Pi-Star']['Version']?> / Dashboard:<?php echo $version; ?></div>
+<div style="font-size: 8px; text-align: right; padding-right: 8px;">Pi-Star:<?php echo $configPistarRelease['Pi-Star']['Version']?> / <?php echo $lang['dashboard']." : ".$version; ?></div>
 <h1>Pi-Star <?php echo $lang['digital_voice']." ".$lang['dashboard_for']." ".$MYCALL; ?></h1>
 <p style="padding-right: 5px; text-align: right; color: #ffffff;">
  <a href="/" style="color: #ffffff;"><?php echo $lang['dashboard'];?></a> |
