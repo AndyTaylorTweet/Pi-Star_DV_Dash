87972db97fa03924daa53a27ea694e8487099713
Andy Taylor <andy@always-online.co.uk>
2017-07-31 12:07:11 +0100

    Update english_uk.php

--- lang/english_uk.php
+++ lang/english_uk.php
@@ -1,7 +1,48 @@
 <?php
 // English UK Language Pack
 $lang = array (
-"Digital Voice"               =>  "Digital Voice",
-"Configuration"               =>  "Configuration"
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Configuration",
+  "dashboard_for"               =>  "Dashboard for",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Update",
+  "backup_restore"              =>  "Backup/Restore",
+  "factory_reset"               =>  "Factory Reset",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware Information",
+  "control_software"            =>  "Control Software",
+  "mmdvmhost_config"            =>  "MMDVMHost Configuration",
+  "general_config"              =>  "General Configuration",
+  "dmr_config"                  =>  "DMR Configuration",
+  "dstar_config"                =>  "D-Star Configuration",
+  "ysf_config"                  =>  "Yaesu System Fusion Configuration",
+  "p25_config"                  =>  "P25 Configuration",
+  "fw_config"                   =>  "Firewall Configuration",
+  "remote_access_pw"            =>  "Remote Access Password",
+  // Config Page - Section General
+  "setting"                     =>  "Setting",
+  "value"                       =>  "Value",
+  "apply"                       =>  "Apply Changes",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostname",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Platform",
+  "cpu_load"                    =>  "CPU Load",
+  "cpu_temp"                    =>  "CPU Temp",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controller Software",
+  "controller_mode"             =>  "Controller Mode",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Mode",
+  "d-star_mode"                 =>  "D-Star Mode",
+  "ysf_mode"                    =>  "YSF Mode",
+  "p25_mode"                    =>  "P25 Mode",
+  "mmdvm_display"               =>  "MMDVM Display Type",
+  "mode_hangtime"               =>  "Mode Hangtime",
+  
 );
 ?>
