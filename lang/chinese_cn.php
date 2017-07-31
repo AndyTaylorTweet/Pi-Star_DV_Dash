a05d9ba7e6fd9c31955da847614393ffb7cc85d3
Andy Taylor <andy@always-online.co.uk>
2017-07-31 23:00:55 +0100

    Create chinese_cn.php
    Imported Chinese translation - thank you Yuan Wang

---
+++ lang/chinese_cn.php
@@ -0,0 +1,121 @@
+<?php
+// Chinese CN (China) Language Pack
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "数字语音",
+  "configuration"               =>  "配置",
+  "dashboard_for"               =>  "仪表盘：",
+  // Banner links
+  "dashboard"                   =>  "仪表盘",
+  "admin"                       =>  "管理",
+  "power"                       =>  "电源",
+  "update"                      =>  "更新",
+  "backup_restore"              =>  "备份/恢复",
+  "factory_reset"               =>  "恢复出厂设置",
+  // Config page section headdings
+  "hardware_info"               =>  "网关硬件信息",
+  "control_software"            =>  "控制软件",
+  "mmdvmhost_config"            =>  "MMDVMHost 配置",
+  "general_config"              =>  "常规配置",
+  "dmr_config"                  =>  "DMR 配置",
+  "dstar_config"                =>  "D-Star 配置",
+  "ysf_config"                  =>  "Yaesu System Fusion 配置",
+  "p25_config"                  =>  "P25 配置",
+  "fw_config"                   =>  "防火墙配置",
+  "remote_access_pw"            =>  "远程访问密码",
+  // Config Page - Section General
+  "setting"                     =>  "设置",
+  "value"                       =>  "设置值",
+  "apply"                       =>  "应用设置",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "主机名",
+  "kernel"                      =>  "内核",
+  "platform"                    =>  "平台",
+  "cpu_load"                    =>  "CPU 负荷",
+  "cpu_temp"                    =>  "CPU 温度",
+  // Config Page - Control Software
+  "controller_software"         =>  "控制器软件",
+  "controller_mode"             =>  "控制器模式",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR 模式",
+  "d-star_mode"                 =>  "D-Star 模式",
+  "ysf_mode"                    =>  "YSF 模式",
+  "p25_mode"                    =>  "P25 模式",
+  "mmdvm_display"               =>  "MMDVM 显示屏类型",
+  "mode_hangtime"               =>  "模式停留时间",
+  // Config Page - General Configuration
+  "node_call"                   =>  "节点呼号",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "电台频率",
+  "lattitude"                   =>  "维度",
+  "longitude"                   =>  "经度",
+  "town"                        =>  "城市",
+  "country"                     =>  "国家",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "电台/调制解调器类型",
+  "node_type"                   =>  "节点类型",
+  "timezone"                    =>  "时区",
+  "dash_lang"                   =>  "控制台语言",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR 主机 (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister 主机 ",
+  "bm_network"                  =>  "BrandMeister Network",
+  "dmr_plus_master"             =>  "DMR+ 主机 ",
+  "dmr_plus_network"            =>  "DMR+ Network",
+  "xlx_master"                  =>  "XLX 主机 ",
+  "xlx_enable"                  =>  "XLX 主机启用",
+  "dmr_cc"                      =>  "DMR 彩色码",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 呼号",
+  "dstar_rpt2"                  =>  "RPT2 呼号",
+  "dstar_irc_password"          =>  "ircDDBGateway 密码",
+  "dstar_default_ref"           =>  "默认反射器",
+  "aprs_host"                   =>  "APRS 服务器",
+  "dstar_irc_lang"              =>  "ircDDBGateway 语言",
+  "dstar_irc_time"              =>  "时间通告",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF 默认服务器",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 默认服务器",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "访问仪表盘",
+  "fw_irc"                      =>  "ircDDGBateway 远程",
+  "fw_ssh"                      =>  "访问SSH",
+  // Config Page - Password
+  "user"                        =>  "用户名",
+  "password"                    =>  "密码",
+  "set_password"                =>  "设置密码",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "模式启动",
+  "net_status"                  =>  "网络状态",
+  "internet"                    =>  "互联网",
+  "radio_info"                  =>  "电台信息",
+  "dstar_repeater"              =>  "D-Star 中继",
+  "dstar_net"                   =>  "D-Star 网络",
+  "dmr_repeater"                =>  "DMR 中继",
+  "dmr_master"                  =>  "DMR 主机",
+  "ysf_net"                     =>  "YSF 网络",
+  "p25_radio"                   =>  "P25 电台",
+  "p25_net"                     =>  "P25 网络",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "时间",
+  "mode"                        =>  "模式",
+  "callsign"                    =>  "呼号",
+  "target"                      =>  "目标",
+  "src"                         =>  "源",        // Short version of "Source"
+  "dur"                         =>  "时长",        // Short version of "Duration"
+  "loss"                        =>  "丢失",
+  "ber"                         =>  "误码率",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "组",
+  "logoff"                      =>  "注销",
+  "info"                        =>  "信息",
+  "utot"                        =>  "用户超时",       // Short for User Timeout
+  "gtot"                        =>  "组超时",       // Short for Group Timeout
+  // Dictionary End
+  "nothing"                     =>  "nothing"
+);
+?>
