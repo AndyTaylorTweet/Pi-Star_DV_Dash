838b8ca68a4d41c2156fa74a1703b02caba29904
Andy Taylor <andy@always-online.co.uk>
2017-08-01 12:03:38 +0100

    Update local_tx.php

--- dstarrepeater/local_tx.php
+++ dstarrepeater/local_tx.php
@@ -1,4 +1,5 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT'].'/config/ircddblocal.php';
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';	      // Translation Code
 $configs = array();
 
 if ($configfile = fopen($gatewayConfigPath,'r')) {
