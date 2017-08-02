a7c65410d9e29877605219accaa1e2bf0533502c
Andy Taylor <andy@always-online.co.uk>
2017-08-02 15:23:41 +0100

    Create norwegian_no.php

---
+++ lang/norwegian_no.php
@@ -0,0 +1,133 @@
+<?php
+//
+// Norwegian NO Language Pack
+// Translated to Norwegian by LB4GH, Torkell Opedal Wullum, lb4gh@lb4gh.no
+// Updated 02-Aug-2017
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Konfigurasjon",
+  "dashboard_for"               =>  "Skrivebord for",
+  // Banner links
+  "dashboard"                   =>  "Skrivebord",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Strøm",
+  "update"                      =>  "Oppdater",
+  "backup_restore"              =>  "Backup/Restore",
+  "factory_reset"               =>  "Tilbakestill",
+  "live_logs"                   =>  "Live Logg",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware Informasjon",
+  "control_software"            =>  "Kontroller Software",
+  "mmdvmhost_config"            =>  "MMDVMHost Konfigurasjon",
+  "general_config"              =>  "Generel Konfigurasjon",
+  "dmr_config"                  =>  "DMR Konfigurasjon",
+  "dstar_config"                =>  "D-Star Konfigurasjon",
+  "ysf_config"                  =>  "Yaesu System Fusion Konfigurasjon",
+  "p25_config"                  =>  "P25 Konfigurasjon",
+  "wifi_config"                 =>  "Trådløs Konfigurasjon",
+  "fw_config"                   =>  "Firewall Konfigurasjon",
+  "remote_access_pw"            =>  "Fjerntilgangs passord",
+  // Config Page - Section General
+  "setting"                     =>  "Innstillinger",
+  "value"                       =>  "Verdi",
+  "apply"                       =>  "Bruk endringer",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostnavn",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Platform",
+  "cpu_load"                    =>  "CPU Load",
+  "cpu_temp"                    =>  "CPU Temp",
+  // Config Page - Control Software
+  "controller_software"         =>  "Kontroller Programvare",
+  "controller_mode"             =>  "Kontroller Mode",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Mode",
+  "d-star_mode"                 =>  "D-Star Mode",
+  "ysf_mode"                    =>  "YSF Mode",
+  "p25_mode"                    =>  "P25 Mode",
+  "mmdvm_display"               =>  "MMDVM Skjerm Type",
+  "mode_hangtime"               =>  "Mode Hangtime",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Node Kallesignal",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Radio Frekvens",
+  "lattitude"                   =>  "Breddegrad",
+  "longitude"                   =>  "lengdegrad",
+  "town"                        =>  "By",
+  "country"                     =>  "Land",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modem Type",
+  "node_type"                   =>  "Node Type",
+  "timezone"                    =>  "System Tids sone",
+  "dash_lang"                   =>  "Skrivebord Språk",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister Nettverk",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ Nettverk",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master Aktivert",
+  "dmr_cc"                      =>  "DMR Farge Kode",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 Kallesignal",
+  "dstar_rpt2"                  =>  "RPT2 Kallesignal",
+  "dstar_irc_password"          =>  "ircDDBGateway Passord",
+  "dstar_default_ref"           =>  "Standard Reflektor",
+  "aprs_host"                   =>  "APRS Vert",
+  "dstar_irc_lang"              =>  "ircDDBGateway Språk",
+  "dstar_irc_time"              =>  "Tids annonsering",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Oppstarts Vert",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Oppstarts vert",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Skrivebord Adgang",
+  "fw_irc"                      =>  "ircDDGBateway Remote",
+  "fw_ssh"                      =>  "SSH Adgang",
+  // Config Page - Password
+  "user"                        =>  "Bruker navn",
+  "password"                    =>  "Passord",
+  "set_password"                =>  "Set Passord",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Modes Aktivert",
+  "net_status"                  =>  "Nettverk Status",
+  "internet"                    =>  "Internett",
+  "radio_info"                  =>  "Radio Info",
+  "dstar_repeater"              =>  "D-Star Repeater",
+  "dstar_net"                   =>  "D-Star Nettverk",
+  "dmr_repeater"                =>  "DMR Repeater",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF Nettverk",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 Nettverk",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Tid",
+  "mode"                        =>  "Mode",
+  "callsign"                    =>  "Kallesignal",
+  "target"                      =>  "Destinasjon",
+  "src"                         =>  "Src",        // Short version of "Source"
+  "dur"                         =>  "Var",        // Short version of "Duration"
+  "loss"                        =>  "Loss",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Gruppe",
+  "logoff"                      =>  "Logg Av",
+  "info"                        =>  "Informasjon",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"              =>  "De siste 20 anropene hørte via denne gatewayen",
+  "local_tx_list"               =>  "Siste 20 samtaler som åpnet denne gatewayen",
+  "active_starnet_groups"       =>  "Aktive Starnet Grupper",
+  "active_starnet_members"      =>  "Aktive Starnet Gruppe medlemer",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Informasjon",
+  "service_status"              =>  "Service Status"
+);
+?>
