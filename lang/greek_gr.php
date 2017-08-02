020030547b5c14d9103c8b5b45b93f7c8a80f155
Andy Taylor <andy@always-online.co.uk>
2017-08-02 22:19:37 +0100

    Create greek_gr.php

---
+++ lang/greek_gr.php
@@ -0,0 +1,129 @@
+<?php
+// Greek GR Language Pack by Demetre SV1UY, version 2
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Ψηφιακή Τηλεφωνία",
+  "configuration"               =>  "Ρυθμίσεις",
+  "dashboard_for"               =>  "Ταμπλό για",
+  // Banner links
+  "dashboard"                   =>  "Ταμπλό",
+  "admin"                       =>  "Διαχείριση",
+  "power"                       =>  "Power",
+  "update"                      =>  "Ενημέρωση",
+  "backup_restore"              =>  "Εφεδρικό Αντίγραφο/Επαναφορά",
+  "factory_reset"               =>  "Εργοστασιακές Ρυθμίσεις",
+  "live_logs"                   =>  "Ημερολόγιο",
+  // Config page section headdings
+  "hardware_info"               =>  "Πληροφορίες Συσκευών Πύλης",
+  "control_software"            =>  "Πρόγραμμα Ελέγχου",
+  "mmdvmhost_config"            =>  "Ρυθμίσεις του MMDVMHost",
+  "general_config"              =>  "Γενικές Ρυθμίσεις",
+  "dmr_config"                  =>  "Ρυθμίσεις DMR",
+  "dstar_config"                =>  "Ρυθμίσεις D-Star",
+  "ysf_config"                  =>  "Ρυθμίσεις Fusion",
+  "p25_config"                  =>  "Ρυθμίσεις P25",
+  "wifi_config"                 =>  "Ρυθμίσεις WiFi",
+  "fw_config"                   =>  "Ρυθμίσεις Τείχους Προστασίας",
+  "remote_access_pw"            =>  "Κωδικός Απομακρυσμένης Πρόσβασης",
+  // Config Page - Section General
+  "setting"                     =>  "Ρυθμίσεις",
+  "value"                       =>  "Τιμή",
+  "apply"                       =>  "Εφαρμογή",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Ονομα Υπολογιστή",
+  "kernel"                      =>  "Πυρήνας",
+  "platform"                    =>  "Πλατφόρμα",
+  "cpu_load"                    =>  "Φόρτος CPU",
+  "cpu_temp"                    =>  "Θερμοκρασία CPU",
+  // Config Page - Control Software
+  "controller_software"         =>  "Πρόγραμμα Ελεγκτή",
+  "controller_mode"             =>  "Κατάσταση Ελεγκτή",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "Λειτουργία DMR",
+  "d-star_mode"                 =>  "Λειτουργία D-Star",
+  "ysf_mode"                    =>  "Λειτουργία YSF",
+  "p25_mode"                    =>  "Λειτουργία P25",
+  "mmdvm_display"               =>  "Τύπος Οθόνης MMDVM",
+  "mode_hangtime"               =>  "Χρόνος Περάτωσης Λειτουργίας",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Διακριτικό Κόμβου",
+  "dmr_id"                      =>  "Ταυτότητα CCS7/DMR",
+  "radio_freq"                  =>  "Συχνότητα",
+  "lattitude"                   =>  "Γεωγραφικό Πλάτος",
+  "longitude"                   =>  "Γεωγραφικό Μήκος",
+  "town"                        =>  "Πόλη",
+  "country"                     =>  "Χώρα",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Τύπος Modem",
+  "node_type"                   =>  "Τύπος Κόμβου",
+  "timezone"                    =>  "Ζώνη Ωρας",
+  "dash_lang"                   =>  "Γλώσσα Πίνακα",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "Κεντρικός Υπολογιστής DMR (MMDVMHost)",
+  "bm_master"                   =>  "Κεντρικός Υπολογιστής BrandMeister",
+  "bm_network"                  =>  "Δίκτυο BrandMeister",
+  "dmr_plus_master"             =>  "Κεντρικός Υπολογιστής DMR+",
+  "dmr_plus_network"            =>  "Δίκτυο DMR+",
+  "xlx_master"                  =>  "Κεντρικός Υπολογιστής XLX",
+  "xlx_enable"                  =>  "Ενεργοποίηση Κεντρικού Υπολογιστή XLX",
+  "dmr_cc"                      =>  "Κωδικός Χρώματος DMR",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "Διακριτικό RPT1",
+  "dstar_rpt2"                  =>  "Διακριτικό RPT2",
+  "dstar_irc_password"          =>  "Κωδικός ircDDBGateway",
+  "dstar_default_ref"           =>  "Reflector Προεπιλογής",
+  "aprs_host"                   =>  "Υπολογιστής APRS",
+  "dstar_irc_lang"              =>  "Γλώσσα ircDDBGateway",
+  "dstar_irc_time"              =>  "Αναγγελίες Ωρας",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "Ύπολογιστής Εκκίνησης YSF",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "Υπολογιστής Εκκίνησης P25",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Πρόσβαση στον Πίνακα",
+  "fw_irc"                      =>  "Απομακρυσμένος ircDDGBateway",
+  "fw_ssh"                      =>  "Πρόσβαση SSH",
+  // Config Page - Password
+  "user"                        =>  "Ονομα Χρήστη",
+  "password"                    =>  "Κωδικός Πρόσβασης",
+  "set_password"                =>  "Ρύθμιση Κωδικού Πρόσβασης",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Ενεργές Λειτουργίες",
+  "net_status"                  =>  "Κατάσταση Δικτύου",
+  "internet"                    =>  "Διαδίκτυο",
+  "radio_info"                  =>  "Πληροφορίες Ασυρμάτου",
+  "dstar_repeater"              =>  "Αναμεταδότης D-Star",
+  "dstar_net"                   =>  "Δίκτυο D-Star",
+  "dmr_repeater"                =>  "Αναμεταδότης DMR",
+  "dmr_master"                  =>  "Κεντρικός Υπολογιστής DMR",
+  "ysf_net"                     =>  "Δίκτυο YSF",
+  "p25_radio"                   =>  "Ασύρματος P25",
+  "p25_net"                     =>  "Δίκτυο P25",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Ωρα",
+  "mode"                        =>  "Λειτουργία",
+  "callsign"                    =>  "Διακριτικό",
+  "target"                      =>  "Προορισμός",
+  "src"                         =>  "Πηγαίος",        // Short version of "Source"
+  "dur"                         =>  "Διάρκεια",        // Short version of "Duration"
+  "loss"                        =>  "Απώλεια",
+  "ber"                         =>  "Σφάλματα",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Ομάδα",
+  "logoff"                      =>  "Αποσύνδεση",
+  "info"                        =>  "Πληροφορίες",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"              =>  "Τελευταία 20 διακριτικά που ακούστηκαν μέσω αυτής της Πύλης",
+  "local_tx_list"               =>  "Τελευταία 20 διακριτικά πο που είχαν πρόσβαση σε αυτή την Πύλη",
+  "active_starnet_groups"       =>  "Ενεργές Ομάδες Starnet",
+  "active_starnet_members"      =>  "Ενεργά Μέλη Ομάδων Starnet",
+  "d-star_link_manager"         =>  "Διαχειριστής Ζεύζης D-Star",
+  "d-star_link_status"          =>  "Πληροφορίες Ζεύξης D-Star",
+  "service_status"              =>  "Κατάσταση Υπηρεσίας"
+);
+?>
