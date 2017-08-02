7ef518c19a023f081ca955476ba8352410298a3b
Andy Taylor <andy@always-online.co.uk>
2017-08-02 14:47:50 +0100

    Create romanian_ro.php

---
+++ lang/romanian_ro.php
@@ -0,0 +1,133 @@
+<?php
+//
+// Romanian RO Language Pack
+// Stirbu Isidor (YO8TEH)
+// 02-Aug-2017
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Voce Digitala",
+  "configuration"               =>  "Configuratii",
+  "dashboard_for"               =>  "Panou Comanda pentru",
+  // Banner links
+  "dashboard"                   =>  "Panou Comanda",
+  "admin"                       =>  "Administrator",
+  "power"                       =>  "Alimentare",
+  "update"                      =>  "Actualizare",
+  "backup_restore"              =>  "Copie Rezerva /Restabilire",
+  "factory_reset"               =>  "Resetare Stare Initiala",
+  "live_logs"                   =>  "Jurnal Live",
+  // Config page section headdings
+  "hardware_info"               =>  "Informatii Hardware Gateway",
+  "control_software"            =>  "Control Software",
+  "mmdvmhost_config"            =>  "Configurare MMDVMHost",
+  "general_config"              =>  "Configurari Generale",
+  "dmr_config"                  =>  "Configurari DMR",
+  "dstar_config"                =>  "Configurari D-Star",
+  "ysf_config"                  =>  "Configurari Yaesu System Fusion",
+  "p25_config"                  =>  "Configurari P25",
+  "wifi_config"                 =>  "Configurari Retea Wireless",
+  "fw_config"                   =>  "Configurari Firewall",
+  "remote_access_pw"            =>  "Parola Acces Exterior",
+  // Config Page - Section General
+  "setting"                     =>  "Setari",
+  "value"                       =>  "Valoare",
+  "apply"                       =>  "Salvare Setari",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Denumire Detinator",
+  "kernel"                      =>  "Nucleu",
+  "platform"                    =>  "Platforma",
+  "cpu_load"                    =>  "Incarcare Procesor",
+  "cpu_temp"                    =>  "Temeratura Procesor",
+  // Config Page - Control Software
+  "controller_software"         =>  "Program de Control",
+  "controller_mode"             =>  "Mod Control",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "Mod DMR",
+  "d-star_mode"                 =>  "Mod D-Star",
+  "ysf_mode"                    =>  "Mod YSF",
+  "p25_mode"                    =>  "Mod P25",
+  "mmdvm_display"               =>  "Tip Display MMDVM",
+  "mode_hangtime"               =>  "Control  Hangtime",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Indicativ Node",
+  "dmr_id"                      =>  "CCS7/ID DMR",
+  "radio_freq"                  =>  "Frecventa Radio",
+  "lattitude"                   =>  "Latitudine",
+  "longitude"                   =>  "Longitudine",
+  "town"                        =>  "Oras",
+  "country"                     =>  "Tara",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Model Radio/Modem",
+  "node_type"                   =>  "Model Node",
+  "timezone"                    =>  "Time Zone Sistem",
+  "dash_lang"                   =>  "Limba Panou Control",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "Reteaua BrandMeister",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "Retea DMR+",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "Activare XLX Master",
+  "dmr_cc"                      =>  "Cod Culoare DMR",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "Indicativ RPT1",
+  "dstar_rpt2"                  =>  "Indicativ RPT2",
+  "dstar_irc_password"          =>  "parola ircDDBGateway",
+  "dstar_default_ref"           =>  "Reflector Implicit ",
+  "aprs_host"                   =>  "Detinator  APRS",
+  "dstar_irc_lang"              =>  "Limba ircDDBGateway",
+  "dstar_irc_time"              =>  "Timp instiintare",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "lansare Detinator YSF",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "lansare Detinator P25",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Acces Panou Control",
+  "fw_irc"                      =>  "Control Exterior ircDDGBateway",
+  "fw_ssh"                      =>  "Acces SSH",
+  // Config Page - Password
+  "user"                        =>  "Nume Utilizator",
+  "password"                    =>  "Parola",
+  "set_password"                =>  "Setare Parola",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Moduri Activate ",
+  "net_status"                  =>  "Stare Retea",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Info Radio",
+  "dstar_repeater"              =>  "Repetor D-Star",
+  "dstar_net"                   =>  "Reteaua D-Star",
+  "dmr_repeater"                =>  "Repetor DMR",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "Reteaua YSF",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "Reteaua P25",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Ora",
+  "mode"                        =>  "Mod",
+  "callsign"                    =>  "Indicativ",
+  "target"                      =>  "Apel Catre",
+  "src"                         =>  "Src",        // Short version of "Source"
+  "dur"                         =>  "Dur",        // Short version of "Duration"
+  "loss"                        =>  "Pierderi",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Grup",
+  "logoff"                      =>  "Deconectare",
+  "info"                        =>  "Informatii",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Ultimele 20 Apeluri Receptionate prin acest Gateway",
+  "local_tx_list"               =>  "Ultimele 20 Apeluri care au accesat acest Gateway",
+  "active_starnet_groups"       =>  "Grupuri Active Starnet",
+  "active_starnet_members"      =>  "Membrii activi in Grupuri Starnet ",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "Informatii Legatura D-Star",
+  "service_status"              =>  "Stare Serviciu"
+);
+?>
