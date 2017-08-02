8914dffae62b61a6ba8de6bd4497b404853e0aca
Andy Taylor <andy@always-online.co.uk>
2017-08-02 16:59:13 +0100

    Create dutch_nl.php

---
+++ lang/dutch_nl.php
@@ -0,0 +1,132 @@
+<?php
+//
+// Dutch NL Language Pack for Pi-Star translated by Rob van Rheenen P0DIB
+// Updated: 02-Aug-2017
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Configuratie",
+  "dashboard_for"               =>  "Dashboard voor",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Update",
+  "backup_restore"              =>  "Backup/Restore",
+  "factory_reset"               =>  "Reset all",
+  "live_logs"                   =>  "Actuele Logs",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway hardware informatie",
+  "control_software"            =>  "Controller Host software",
+  "mmdvmhost_config"            =>  "MMDVMHost configuratie",
+  "general_config"              =>  "Algemene configuratie",
+  "dmr_config"                  =>  "DMR configuratie",
+  "dstar_config"                =>  "D-Star configuratie",
+  "ysf_config"                  =>  "Yaesu System Fusion configuratie",
+  "p25_config"                  =>  "P25 configuratie",
+  "wifi_config"                 =>  "Wireless configuratie",
+  "fw_config"                   =>  "Firewall configuratie",
+  "remote_access_pw"            =>  "Remote Access paswoord",
+  // Config Page - Section General
+  "setting"                     =>  "Instelling",
+  "value"                       =>  "Waarde",
+  "apply"                       =>  "Toepassen",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostnaam",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Platform",
+  "cpu_load"                    =>  "CPU Load",
+  "cpu_temp"                    =>  "CPU Temp",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controller software",
+  "controller_mode"             =>  "Controller modus",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR modus",
+  "d-star_mode"                 =>  "D-Star modus",
+  "ysf_mode"                    =>  "YSF modus",
+  "p25_mode"                    =>  "P25 modus",
+  "mmdvm_display"               =>  "MMDVM display type",
+  "mode_hangtime"               =>  "Mode hangtijd",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Node Callsign",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Radio frequentie",
+  "lattitude"                   =>  "Breedtegraad",
+  "longitude"                   =>  "Lengtegraad",
+  "town"                        =>  "Plaats",
+  "country"                     =>  "Land",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modem type",
+  "node_type"                   =>  "Node type",
+  "timezone"                    =>  "Systeem tijdzone",
+  "dash_lang"                   =>  "Dashboard taal",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister netwerk",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ netwerk",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master AAN",
+  "dmr_cc"                      =>  "DMR kleur code (CC)",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 Callsign",
+  "dstar_rpt2"                  =>  "RPT2 Callsign",
+  "dstar_irc_password"          =>  "ircDDBGateway paswoord",
+  "dstar_default_ref"           =>  "Standaard Reflector",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "ircDDBGateway taal",
+  "dstar_irc_time"              =>  "Tijd uitzenden (D-Star)",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF standaard Host",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 standaard Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Dashboard toegang",
+  "fw_irc"                      =>  "ircDDGBateway op afstand",
+  "fw_ssh"                      =>  "SSH toegang",
+  // Config Page - Password
+  "user"                        =>  "Gebruikersnaam",
+  "password"                    =>  "Paswoord",
+  "set_password"                =>  "Voer paswoord in",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Actieve modi",
+  "net_status"                  =>  "Netwerk status",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Radio info",
+  "dstar_repeater"              =>  "D-Star repeater",
+  "dstar_net"                   =>  "D-Star netwerk",
+  "dmr_repeater"                =>  "DMR repeater",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF netwerk",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 netwerk",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Tijd",
+  "mode"                        =>  "Mode",
+  "callsign"                    =>  "Callsign",
+  "target"                      =>  "Doel",
+  "src"                         =>  "Bron",        // Short version of "Source"
+  "dur"                         =>  "Duur",        // Short version of "Duration"
+  "loss"                        =>  "Loss",        // Not translated due space (most NL understand Loss) 
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Groep",
+  "logoff"                      =>  "Uitloggen",
+  "info"                        =>  "Informatie",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"              => "Laatste 20 calls gehoord via deze Gateway",
+  "local_tx_list"               =>  "Laatste 20 calls TX via deze Gateway",
+  "active_starnet_groups"       =>  "Actieve Starnet groepen",
+  "active_starnet_members"      =>  "Actieve Starnet groepsleden",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Informatie",
+  "service_status"              =>  "Service Status"
+);
+?>
