6eda74932a30a6cb90dedae96c7aa472b7996ead
Andy Taylor <andy@always-online.co.uk>
2017-08-03 12:12:54 +0100

    Create german_de.php

---
+++ lang/german_de.php
@@ -0,0 +1,134 @@
+<?php
+//
+// German Language Pack
+// by Klaus, DL5RFK
+// 03-Aug-2017
+// file: /var/www/dashboard/lang/german_de.php
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Konfiguration",
+  "dashboard_for"               =>  "Dashboard f&uuml;r",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Aktuallisieren",
+  "backup_restore"              =>  "Datensicherung/Wiederherstellung",
+  "factory_reset"               =>  "Werkseinstellung",
+  "live_logs"                   =>  "Protokoll",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware Information",
+  "control_software"            =>  "Kontrol Software",
+  "mmdvmhost_config"            =>  "MMDVMHost Konfiguration",
+  "general_config"              =>  "Basis Konfiguration",
+  "dmr_config"                  =>  "DMR Konfiguration",
+  "dstar_config"                =>  "D-Star Konfiguration",
+  "ysf_config"                  =>  "Yaesu System Fusion Konfiguration",
+  "p25_config"                  =>  "P25 Konfiguration",
+  "wifi_config"                 =>  "Wireless Konfiguration",
+  "fw_config"                   =>  "Firewall Konfiguration",
+  "remote_access_pw"            =>  "Fernzugriff Passwort",
+  // Config Page - Section General
+  "setting"                     =>  "Einstellung",
+  "value"                       =>  "Wert",
+  "apply"                       =>  "Speichern",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Rechnername",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Plattform",
+  "cpu_load"                    =>  "CPU Last",
+  "cpu_temp"                    =>  "CPU Temp",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controller Software",
+  "controller_mode"             =>  "Controller Mode",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Modus",
+  "d-star_mode"                 =>  "D-Star Modus",
+  "ysf_mode"                    =>  "YSF Modus",
+  "p25_mode"                    =>  "P25 Modus",
+  "mmdvm_display"               =>  "MMDVM Display Typ",
+  "mode_hangtime"               =>  "Mode Hangtime",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Node Rufzeichen",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Radio Frequenz",
+  "lattitude"                   =>  "Breitengrad",
+  "longitude"                   =>  "L&auml;ngengrad",
+  "town"                        =>  "Stadt",
+  "country"                     =>  "Land",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modem Typ",
+  "node_type"                   =>  "Node Typ",
+  "timezone"                    =>  "Systemzeit Zone",
+  "dash_lang"                   =>  "Dashboard Sprache",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister Netzwerk",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ Netzwerk",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master Aktiv",
+  "dmr_cc"                      =>  "DMR Colour Code",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 Rufzeichen",
+  "dstar_rpt2"                  =>  "RPT2 Rufzeichen",
+  "dstar_irc_password"          =>  "ircDDBGateway Passwort",
+  "dstar_default_ref"           =>  "Standard Reflektor",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "ircDDBGateway Sprache",
+  "dstar_irc_time"              =>  "Zeit Ansagen",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Startup Host",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Startup Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Dashboard Zugriff",
+  "fw_irc"                      =>  "ircDDGBateway Remote",
+  "fw_ssh"                      =>  "SSH Zugriff",
+  // Config Page - Password
+  "user"                        =>  "Benutzername",
+  "password"                    =>  "Passwort",
+  "set_password"                =>  "Passwort setzen",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Aktive Modi",
+  "net_status"                  =>  "Netzwerk Status",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Radio Info",
+  "dstar_repeater"              =>  "D-Star Relais",
+  "dstar_net"                   =>  "D-Star Netzwerk",
+  "dmr_repeater"                =>  "DMR Relais",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF Netzwerk",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 Netzwerk",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Zeit",
+  "mode"                        =>  "Mode",
+  "callsign"                    =>  "Rufzeichen",
+  "target"                      =>  "Ziel",
+  "src"                         =>  "Quelle",        // Short version of "Source"
+  "dur"                         =>  "Dauer",        // Short version of "Duration"
+  "loss"                        =>  "Verlust",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Gruppe",
+  "logoff"                      =>  "LogOff",
+  "info"                        =>  "Information",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Letzten 20 Rufzeichen, die gerh&ouml;rt wurden",
+  "local_tx_list"               =>  "Letzen 20 Rufzeichen, die dieses Gateway nutzen",
+  "active_starnet_groups"       =>  "Aktive Starnet Gruppen",
+  "active_starnet_members"      =>  "Aktive Starnet Gruppen Mitglieder",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Information",
+  "service_status"              =>  "Service Status"
+);
+?>
