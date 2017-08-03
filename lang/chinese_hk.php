72c62a59f14ec27ec13893633d6df9e68874fe2b
Andy Taylor <andy@always-online.co.uk>
2017-08-03 20:48:05 +0100

    Create chinese_hk.php

---
+++ lang/chinese_hk.php
@@ -0,0 +1,133 @@
+<?php
+// Chinese HK (Traditional Chinese characters) Language Pack
+// Translated by Le Peng（BD7KLE）, Email: bd7kle@gmail.com
+//               Yuan Wang（BG3MDO）, Email: bg3mdo@gmail.com
+// Maintained by Yuan Wang（BG3MDO）, Email: bg3mdo@gmail.com 
+// Last update on 03/08/2017
+
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "數字語音",
+  "configuration"               =>  "設定",
+  "dashboard_for"               =>  "儀錶盤 -",
+  // Banner links
+  "dashboard"                   =>  "儀錶盤",
+  "admin"                       =>  "管理",
+  "power"                       =>  "電源",
+  "update"                      =>  "更新",
+  "backup_restore"              =>  "備份/恢復",
+  "factory_reset"               =>  "恢復出廠設定",
+  "live_logs"                   =>  "日誌",
+  // Config page section headdings
+  "hardware_info"               =>  "網關硬體信息",
+  "control_software"            =>  "控制軟體",
+  "mmdvmhost_config"            =>  "MMDVMHost 設定",
+  "general_config"              =>  "常規設定",
+  "dmr_config"                  =>  "DMR 設定",
+  "dstar_config"                =>  "D-Star 設定",
+  "ysf_config"                  =>  "Yaesu System Fusion 設定",
+  "p25_config"                  =>  "P25 設定",
+  "fw_config"                   =>  "防火墻設定",
+  "remote_access_pw"            =>  "遠程訪問密碼",
+  // Config Page - Section General
+  "setting"                     =>  "設定",
+  "value"                       =>  "設定值",
+  "apply"                       =>  "應用設定",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "主機名",
+  "kernel"                      =>  "內核",
+  "platform"                    =>  "平臺",
+  "cpu_load"                    =>  "CPU 負荷",
+  "cpu_temp"                    =>  "CPU 溫度",
+  // Config Page - Control Software
+  "controller_software"         =>  "控制器軟體",
+  "controller_mode"             =>  "控制器模式",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR 模式",
+  "d-star_mode"                 =>  "D-Star 模式",
+  "ysf_mode"                    =>  "YSF 模式",
+  "p25_mode"                    =>  "P25 模式",
+  "mmdvm_display"               =>  "MMDVM 顯示器類型",
+  "mode_hangtime"               =>  "模式停留時間",
+  // Config Page - General Configuration
+  "node_call"                   =>  "節點呼號",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "電臺頻率",
+  "lattitude"                   =>  "緯度",
+  "longitude"                   =>  "經度",
+  "town"                        =>  "區域",
+  "country"                     =>  "國家",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "電臺/調製解調器類型",
+  "node_type"                   =>  "節點類型",
+  "timezone"                    =>  "時區",
+  "dash_lang"                   =>  "儀錶盤語言",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR 主機 (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister 主機 ",
+  "bm_network"                  =>  "BrandMeister 網絡",
+  "dmr_plus_master"             =>  "DMR+ 主機 ",
+  "dmr_plus_network"            =>  "DMR+ 網絡",
+  "xlx_master"                  =>  "XLX 主機 ",
+  "xlx_enable"                  =>  "XLX 主機啟用",
+  "dmr_cc"                      =>  "DMR 彩色碼",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 呼號",
+  "dstar_rpt2"                  =>  "RPT2 呼號",
+  "dstar_irc_password"          =>  "ircDDBGateway 密碼",
+  "dstar_default_ref"           =>  "默認反射器",
+  "aprs_host"                   =>  "APRS 服務器",
+  "dstar_irc_lang"              =>  "ircDDBGateway 語言",
+  "dstar_irc_time"              =>  "時間通告",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF 默認服務器",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 默認服務器",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "儀表盤訪問",
+  "fw_irc"                      =>  "ircDDBGateway 遠程",
+  "fw_ssh"                      =>  "SSH訪問",
+  // Config Page - Password
+  "user"                        =>  "用戶名",
+  "password"                    =>  "密碼",
+  "set_password"                =>  "密碼設定",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "啟用的模式",
+  "net_status"                  =>  "網絡狀態",
+  "internet"                    =>  "互聯網",
+  "radio_info"                  =>  "電臺信息",
+  "dstar_repeater"              =>  "D-Star 中繼",
+  "dstar_net"                   =>  "D-Star 網絡",
+  "dmr_repeater"                =>  "DMR 中繼",
+  "dmr_master"                  =>  "DMR 主機",
+  "ysf_net"                     =>  "YSF 網絡",
+  "p25_radio"                   =>  "P25 電臺",
+  "p25_net"                     =>  "P25 網絡",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "時間",
+  "mode"                        =>  "模式",
+  "callsign"                    =>  "呼號",
+  "target"                      =>  "目標",
+  "src"                         =>  "源",        // Short version of "Source"
+  "dur"                         =>  "時長",      // Short version of "Duration"
+  "loss"                        =>  "丟失",
+  "ber"                         =>  "誤碼率",    // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "組",
+  "logoff"                      =>  "註銷",
+  "info"                        =>  "信息",
+  "utot"                        =>  "用户超時",     // Short for User Timeout
+  "gtot"                        =>  "組超時",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "網關上最後 20 個呼叫",
+  "local_tx_list"               =>  "最後 20 個本地呼叫",
+  "active_starnet_groups"       =>  "激活 Starnet 組",
+  "active_starnet_members"      =>  "激活 Starnet 組成員",
+  "d-star_link_manager"         =>  "D-Star 連接管理器",
+  "d-star_link_status"          =>  "D-Star 連接信息",
+  "service_status"              =>  "服務狀態"
+);
+?>
