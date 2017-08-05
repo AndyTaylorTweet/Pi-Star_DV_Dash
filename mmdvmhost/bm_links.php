bce552e9dea13894f75eb6ed47041c65c12feb83
Andy Taylor <andy@always-online.co.uk>
2017-08-05 21:45:09 +0100

    Update bm_links.php

--- mmdvmhost/bm_links.php
+++ mmdvmhost/bm_links.php
@@ -33,34 +33,42 @@ if ( $testMMDVModeDMR == 1 ) {
   // DMR ID, we will need this for the JSON lookup
   $dmrID = getConfigItem("DMR", "Id", $mmdvmconfigs);
 
-  // Connect to the master and get the JSON for this host on the correct DMR Master
-  $json = json_decode(file_get_contents("http://$dmrMasterHostIP/status/status.php", true));
-  foreach($json as $repeater) {
-    if($repeater->number == $dmrID) {
-      $taData = $repeater->caption;
-      $values = $repeater->values;
-    }
-  }
-
-  if ($values[18] == '0') { $linkedTG = "None"; } else { $linkedTG = $values[18]; }
-  if (($values[19] >= '4001') && ($values[19] <= '4999')) { $linkedREF = $values[19]; } else { $linkedREF = "None"; }
+  // Use BM API to get information about current TGs
+  $json = json_decode(file_get_contents("https://api.brandmeister.network/v1.0/repeater/?action=profile&q=$dmrID", true));
+  if (isset($json->reflector->reflector)) { $bmReflectorDef = $json->reflector->reflector; } else { $bmReflectorDef = "Not Set"; }
+  if (isset($json->reflector->interval)) { $bmReflectorInterval = $json->reflector->interval; } else {$bmReflectorInterval = "Not Set"; }
+  if (isset($json->reflector->active)) { $bmReflectorActive = $json->reflector->active; } else { $bmReflectorActive = "None"; }
+  if (isset($json->staticSubscriptions)) { $bmStaticTGListJson = $json->staticSubscriptions;
+                                                foreach($bmStaticTGListJson as $staticTG) {
+                                                        $bmStaticTGList .= $staticTG->talkgroup." ";
+                                                }
+                                        $bmStaticTGList = wordwrap($bmStaticTGList, 15, "<br />\n");
+                                        } else { $bmStaticTGList = "None"; }
+  if (isset($json->dynamicSubscription)) { $bmDynamicTGList = $json->dynamicSubscription;
+                                                foreach($bmDynamicTGListJson as $dynamicTG) {
+                                                        $bmDynamicTGList .= $dynamicTG->talkgroup." ";
+                                                }
+                                        $bmDynamicTGList = wordwrap($bmDynamicTGList, 15, "<br />\n");
+                                        } else { $bmDynamicTGList = "None"; }
 
   echo '<b>Active BrandMeister Connections</b>
   <table>
     <tr>
       <th><a class=tooltip href="#">DMR Master<span><b>Connected Master</b></span></a></th>
-      <th><a class=tooltip href="#">Repeater / Hotspot ID<span><b>DMR ID for this Node</b></span></a></th>
-      <th><a class=tooltip href="#">Talker Alias Data<span><b>TA Data</b></span></a></th>
-      <!-- <th><a class=tooltip href="#">Linked TG<span><b>Last Linked Talk Group</b></span></a></th> -->
-      <th><a class=tooltip href="#">Linked Reflector<span><b>Last Linked Reflector</b></span></a></th>
+      <th><a class=tooltip href="#">Default Ref<span><b>Default Reflector</b></span></a></th>
+      <th><a class=tooltip href="#">Timeout<span><b>Configured Timeout</b></span></a></th>
+      <th><a class=tooltip href="#">Active Ref<span><b>Active Reflector</b></span></a></th>
+      <th><a class=tooltip href="#">Static TGs<span><b>Statically linked talkgroups</b></span></a></th>
+      <th><a class=tooltip href="#">Dynamic TGs<span><b>Dynamically linked talkgroups</b></span></a></th>
     </tr>'."\n";
 
   echo '    <tr>'."\n";
   echo '      <td>'.$dmrMasterHost.'</td>';
-  echo '<td>'.$dmrID.'</td>';
-  echo '<td>'.$taData.'</td>';
-  //echo '<td>'.$linkedTG.'</td>';
-  echo '<td>'.$linkedREF.'</td>'."\n";
+  echo '<td>'.$bmReflectorDef.'</td>';
+  echo '<td>'.$bmReflectorInterval.'</td>';
+  echo '<td>'.$bmReflectorActive.'</td>';
+  echo '<td>'.$bmStaticTGList.'</td>';
+  echo '<td>'.$bmDynamicTGList.'</td>';
   echo '    </tr>'."\n";
 
   echo '  </table>'."\n";
