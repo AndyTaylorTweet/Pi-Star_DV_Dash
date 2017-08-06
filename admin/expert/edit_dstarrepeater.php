bfbbfd6c3627967b4c0dee329ad6937d5421d86f
Andy Taylor <andy@always-online.co.uk>
2017-08-06 18:03:51 +0100

    Update edit_dstarrepeater.php

--- admin/expert/edit_dstarrepeater.php
+++ admin/expert/edit_dstarrepeater.php
@@ -37,6 +37,17 @@ require_once('../config/version.php');
     <a href="/admin/config_backup.php" style="color: #ffffff;"><?php echo $lang['backup_restore'];?></a> |
     <a href="/admin/configure.php" style="color: #ffffff;"><?php echo $lang['configuration'];?></a>
   </p>
+  <p style="padding-right: 5px; text-align: right; color: #ffffff;">
+    Edit:
+    <a href="edit_dstarrepeater.php" style="color: #ffffff;">DStarRepeater</a> |
+    <a href="edit_mmdvmhost.php" style="color: #ffffff;">MMDVMHost</a> |
+    <a href="edit_dmrgateway.php" style="color: #ffffff;">DMRGateway</a> |
+    <a href="edit_ysfgateway.php" style="color: #ffffff;">YSFGateway</a> |
+    <a href="edit_p25gateway.php" style="color: #ffffff;">P25Gateway</a> |
+    <a href="edit_ircddbgateway.php" style="color: #ffffff;">ircDDBGateway</a> |
+    <a href="edit_starnetserver.php" style="color: #ffffff;">StarNetServer</a> |
+    <a href="edit_timeserver.php" style="color: #ffffff;">TimeServer</a>
+  </p>
   </div>
   <div class="contentwide">
 
