02fa3f640ca495ef181dd805700e460649457eaa
Andy Taylor <andy@always-online.co.uk>
2017-08-20 14:25:34 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -69,6 +69,24 @@ if (file_exists('/etc/dstar-radio.mmdvmhost')) {
 	if (fopen($modemConfigFileMMDVMHost,'r')) { $configModem = parse_ini_file($modemConfigFileMMDVMHost, true); }
 }
 
+function aprspass ($callsign) { 
+	$stophere = strpos($callsign, '-'); 
+	if ($stophere) $callsign = substr($callsign, 0, $stophere); 
+	$realcall = strtoupper(substr($callsign, 0, 10)); 
+	// initialize hash 
+	$hash = 0x73e2; 
+	$i = 0; 
+	$len = strlen($realcall); 
+	// hash callsign two bytes at a time 
+	while ($i < $len) { 
+		$hash ^= ord(substr($realcall, $i, 1))<<8; 
+		$hash ^= ord(substr($realcall, $i + 1, 1)); 
+		$i += 2; 
+	} 
+	// mask off the high bit so number is always positive 
+	return $hash & 0x7fff; 
+}
+
 $progname = basename($_SERVER['SCRIPT_FILENAME'],".php");
 $rev=$version;
 $MYCALL=strtoupper($callsign);
