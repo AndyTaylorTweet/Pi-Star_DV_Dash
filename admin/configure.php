e7a25a28c1048024462c0967e9400f8cfe73dab1
Andy Taylor <andy@always-online.co.uk>
2017-08-20 14:23:57 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -591,6 +591,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	if (empty($_POST['dmrId']) != TRUE ) {
 	  $newPostDmrId = preg_replace('/[^0-9]/', '', $_POST['dmrId']);
 	  $configmmdvm['DMR']['Id'] = $newPostDmrId;
+	  $configmmdvm['General']['Id'] = $newPostDmrId;
 	  $configdmrgateway['XLX Network 1']['Id'] = substr($newPostDmrId,0,7);
 	}
 
