d2c1b2cdbfd0af5a4a77097d30fa55bd407db00a
Andy Taylor <andy@always-online.co.uk>
2017-08-26 17:52:39 +0100

    Update sysinfo.php

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -125,18 +125,18 @@ function formatSize( $bytes ) {
   </div>
   <div class="contentwide">
   <table width="100%" border="0">
-  <tr><th colspan="3">Pi-Star System Information</th></tr>
+  <tr><th colspan="2">Pi-Star System Information</th></tr>
 <?php
 // Ram information
 if ($system['mem_info']) {
     $sysRamUsed = $system['mem_info']['MemTotal'] - $system['mem_info']['MemFree'] - $system['mem_info']['Buffers'] - $system['mem_info']['Cached'];
     $sysRamPercent = sprintf('%.2f',($sysRamUsed / $system['mem_info']['MemTotal']) * 100);
-    echo "  <tr><td><b>Memory</b></td><td>RAM</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$sysRamPercent."%;'>Used&nbsp;".$sysRamPercent."%</div></div>";
+    echo "  <tr><td>RAM</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$sysRamPercent."%;'>Used&nbsp;".$sysRamPercent."%</div></div>";
     echo "  <b>Total:</b> ".formatSize($system['mem_info']['MemTotal'])."<b> Used:</b> ".formatSize($sysRamUsed)."<b> Free:</b> ".formatSize($system['mem_info']['MemTotal'] - $sysRamUsed)."</td></tr>\n";
 }
 // Filesystem Information
 if (count($system['partitions']) > 0) {
-    echo "  <tr><td><b>Disk</b></td><td><b>Mount</b></td><td><b>Stats</b></td></tr>\n";
+    echo "  <tr><td><b>Mount</b></td><td><b>Stats</b></td></tr>\n";
     foreach($system['partitions'] as $fs) {
         if ($fs['Used']['value'] > 0 && $fs['FileSystem']['text']!= "none" && $fs['FileSystem']['text']!= "udev") {
             $diskFree = $fs['Free']['value'];
@@ -144,7 +144,7 @@ if (count($system['partitions']) > 0) {
             $diskUsed = $fs['Used']['value'];
             $diskPercent = sprintf('%.2f',($diskUsed / $diskTotal) * 100);
                         
-            echo "  <tr><td></td><td align=\"left\">".$fs['Partition']['text']."</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$diskPercent."%;'>Used&nbsp;".$diskPercent."%</div></div>";
+            echo "  <tr><td align=\"left\">".$fs['Partition']['text']."</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$diskPercent."%;'>Used&nbsp;".$diskPercent."%</div></div>";
             echo "  <b>Total:</b> ".formatSize($diskTotal)."<b> Used:</b> ".formatSize($diskUsed)."<b> Free:</b> ".formatSize($diskFree)."</td></tr>\n";
         }
     }
