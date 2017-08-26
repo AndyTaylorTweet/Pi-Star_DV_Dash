29eff77805e71c51f1415c8eea48e966b208964b
Andy Taylor <andy@always-online.co.uk>
2017-08-26 17:43:59 +0100

    Update sysinfo.php

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -138,24 +138,16 @@ function formatSize( $bytes ) {
   <div class="contentwide">
   <table width="100%" border="0">
 <?php
-echo "  <tr><td><b>Pi</b></td><td>CPU Temp</td><td>".number_format((int)@exec('cat /sys/class/thermal/thermal_zone0/temp')/1000, '2', '.', '')."&degC</td></tr>\n";
-              foreach (glob("/boot/emonSD-*") as $emonpiRelease) {
-                $emonpiRelease = str_replace("/boot/", '', $emonpiRelease);
-              }
-              if (isset($emonpiRelease)) {
-                echo "  <tr><td></td><td>Release</td><td>".$emonpiRelease."</td></tr>\n";
-                echo "  <tr><td></td><td>File-system</td><td>Set root file-system temporarily to read-write, (default read-only)<button id=\"fs-rw\" class=\"btn btn-danger btn-small pull-right\">"._('Read-Write')."</button> <button id=\"fs-ro\" class=\"btn btn-info btn-small pull-right\">"._('Read-Only')."</button></td></tr>\n";
-}
 // Ram information
 if ($system['mem_info']) {
               $sysRamUsed = $system['mem_info']['MemTotal'] - $system['mem_info']['MemFree'] - $system['mem_info']['Buffers'] - $system['mem_info']['Cached'];
               $sysRamPercent = sprintf('%.2f',($sysRamUsed / $system['mem_info']['MemTotal']) * 100);
-              echo "  <tr><td><b>Memory</b></td><td>RAM</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$sysRamPercent."%;'>Used&nbsp;".$sysRamPercent."%</div></div>";
+              echo "  <tr><td align=\"left\"><b>Memory</b></td><td td align=\"left\">RAM</td><td><div class='progress progress-info' style='margin-bottom: 0;'><div class='bar' style='width: ".$sysRamPercent."%;'>Used&nbsp;".$sysRamPercent."%</div></div>";
               echo "  <b>Total:</b> ".formatSize($system['mem_info']['MemTotal'])."<b> Used:</b> ".formatSize($sysRamUsed)."<b> Free:</b> ".formatSize($system['mem_info']['MemTotal'] - $sysRamUsed)."</td></tr>\n";
 }
 // Filesystem Information
 if (count($system['partitions']) > 0) {
-    echo "  <tr><td><b>Disk</b></td><td><b>Mount</b></td><td><b>Stats</b></td></tr>\n";
+    echo "  <tr><td td align=\"left\"><b>Disk</b></td><td td align=\"left\"><b>Mount</b></td><td><b>Stats</b></td></tr>\n";
     foreach($system['partitions'] as $fs) {
         if ($fs['Used']['value'] > 0 && $fs['FileSystem']['text']!= "none" && $fs['FileSystem']['text']!= "udev") {
             $diskFree = $fs['Free']['value'];
