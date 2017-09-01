29b9c89a057d7af75ea8aaf8dbd9e7785e75f2aa
Andy Taylor <andy@always-online.co.uk>
2017-09-01 18:55:11 +0100

    Update edit_mmdvmhost.php

--- admin/expert/edit_mmdvmhost.php
+++ admin/expert/edit_mmdvmhost.php
@@ -117,8 +117,6 @@ if($_POST) {
 		return $success;
 	}
 
-echo "<html>\n<body>";
-
 //parse the ini file using default parse_ini_file() PHP function
 $parsed_ini = parse_ini_file($filepath, true);
 
