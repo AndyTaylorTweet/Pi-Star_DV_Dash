5145361e061598089d08b55e93422520b94765c4
Andy Taylor <andy@always-online.co.uk>
2017-09-05 15:41:32 +0100

    Update ssh_access.php

--- admin/expert/ssh_access.php
+++ admin/expert/ssh_access.php
@@ -70,6 +70,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/expert/ssh_access.php") {
     } ?>
   </div></td></tr>
   </table>
+  <?php if (isset($shellPort)) { echo "<a href=\"https://".$_SERVER['HTTP_HOST'].":".$shellPort."\">Click here for fullscreen SSH client"; } ?>
   </div>
   <div class="footer">
   Pi-Star web config, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
