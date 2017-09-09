6cc65819a9ff5b381879d7601a35c4eb8284b5c7
Andy Taylor <andy@always-online.co.uk>
2017-09-09 10:15:36 +0100

    Add files via upload

---
+++ lang/japanese_jp.php
@@ -0,0 +1,133 @@
+<?php
+//
+// English UK Language Pack
+// Andy Taylor (MW0MWZ)
+// Updated: 02-Aug-
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Configuration",
+  "dashboard_for"               =>  "Dashboard for",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Update",
+  "backup_restore"              =>  "Backup/Restore",
+  "factory_reset"               =>  "Factory Reset",
+  "live_logs"                   =>  "Live Logs",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware Information",
+  "control_software"            =>  "Control Software",
+  "mmdvmhost_config"            =>  "MMDVMHost Configuration",
+  "general_config"              =>  "General Configuration",
+  "dmr_config"                  =>  "DMR Configuration",
+  "dstar_config"                =>  "D-Star Configuration",
+  "ysf_config"                  =>  "Yaesu System Fusion Configuration",
+  "p25_config"                  =>  "P25 Configuration",
+  "wifi_config"                 =>  "Wireless Configuration",
+  "fw_config"                   =>  "Firewall Configuration",
+  "remote_access_pw"            =>  "Remote Access Password",
+  // Config Page - Section General
+  "setting"                     =>  "設定項目",
+  "value"                       =>  "設定内容",
+  "apply"                       =>  "更新",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "ホストネーム",
+  "kernel"                      =>  "カーネル",
+  "platform"                    =>  "プラットフォーム",
+  "cpu_load"                    =>  "CPU負荷",
+  "cpu_temp"                    =>  "CPU温度",
+  // Config Page - Control Software
+  "controller_software"         =>  "コントロールソフト",
+  "controller_mode"             =>  "コントロールモード",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR モード",
+  "d-star_mode"                 =>  "D-Star モード",
+  "ysf_mode"                    =>  "YSF モード",
+  "p25_mode"                    =>  "P25 モード",
+  "mmdvm_display"               =>  "MMDVM ディスプレイタイプ",
+  "mode_hangtime"               =>  "モードハングタイム",
+  // Config Page - General Configuration
+  "node_call"                   =>  "ノード コールサイン",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "周波数",
+  "lattitude"                   =>  "緯度",
+  "longitude"                   =>  "経度",
+  "town"                        =>  "市町村",
+  "country"                     =>  "国",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modem タイプ",
+  "node_type"                   =>  "ノードタイプ",
+  "timezone"                    =>  "システム タイムゾーン",
+  "dash_lang"                   =>  "ダッシュボード言語",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister Network",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ Network",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master Enable",
+  "dmr_cc"                      =>  "DMR Colour Code",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 コールサイン",
+  "dstar_rpt2"                  =>  "RPT2 コールサイン",
+  "dstar_irc_password"          =>  "ircDDBGateway パスワード",
+  "dstar_default_ref"           =>  "ディフォルト　リフレクター",
+  "aprs_host"                   =>  "APRS ホスト",
+  "dstar_irc_lang"              =>  "ircDDBGateway 言語",
+  "dstar_irc_time"              =>  "時刻アナウンス",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF スタートアップ　ホスト",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 スタートアップ　ホスト",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "ダッシュボード　アクセス",
+  "fw_irc"                      =>  "ircDDGBateway リモート",
+  "fw_ssh"                      =>  "SSH アクセス",
+  // Config Page - Password
+  "user"                        =>  "ユーザー名",
+  "password"                    =>  "パスワード",
+  "set_password"                =>  "パスワード保存",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "使用可能モード",
+  "net_status"                  =>  "ネットワーク状況",
+  "internet"                    =>  "インターネット",
+  "radio_info"                  =>  "Radio Info",
+  "dstar_repeater"              =>  "D-Star Repeater",
+  "dstar_net"                   =>  "D-Star Network",
+  "dmr_repeater"                =>  "DMR Repeater",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF Network",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 Network",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "時刻",
+  "mode"                        =>  "モード",
+  "callsign"                    =>  "コールサイン",
+  "target"                      =>  "ターゲット",
+  "src"                         =>  "Src",        // Short version of "Source"
+  "dur"                         =>  "Dur",        // Short version of "Duration"
+  "loss"                        =>  "Loss",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // Dashboard - Extra Info
+  "group"                       =>  "Group",
+  "logoff"                      =>  "LogOff",
+  "info"                        =>  "Information",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Last 20 calls heard via this Gateway",
+  "local_tx_list"               =>  "Last 20 calls that accessed this Gateway",
+  "active_starnet_groups"       =>  "Active Starnet Groups",
+  "active_starnet_members"      =>  "Active Starnet Group Members",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Information",
+  "service_status"              =>  "Service Status"
+);
+?>
