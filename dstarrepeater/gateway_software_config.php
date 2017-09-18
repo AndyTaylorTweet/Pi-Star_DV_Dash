74680247bf9ce518d46f8058693aec2c57720588
Andy Taylor <andy@always-online.co.uk>
2017-09-18 12:45:10 +0100

    Update gateway_software_config.php

--- dstarrepeater/gateway_software_config.php
+++ dstarrepeater/gateway_software_config.php
@@ -4,15 +4,15 @@
   <td><?php print $configs[ircddbHostname]; ?></td>
   <td><?php if($configs['aprsEnabled'] == 1){ print $configs[aprsHostname]; } else { print "<img src=\"images/20red.png\">";} ?></td>
   <?php
-  if($configs['ccsEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['dcsEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['dextraEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /><td>"; }
-  if($configs['dplusEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['dratsEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['infoEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['ircddbEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['echoEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
-  if($configs['logEnabled'] == 1){print "<td><img src=\"images/20green.png\" /></td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['ccsEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['dcsEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['dextraEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /><td>"; }
+  if($configs['dplusEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['dratsEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['infoEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['ircddbEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['echoEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
+  if($configs['logEnabled'] == 1){print "<td style=\"bgcolor:#0b0;\">ON</td>"; } else { print "<td><img src=\"images/20red.png\" /></td>"; }
   ?>
 </tr>
 </table>
