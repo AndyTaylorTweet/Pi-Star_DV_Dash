3209c387fc247f228ab434b90db0c51ec7b0f545
Andy Taylor <andy@always-online.co.uk>
2017-09-28 00:51:32 +0100

    Create spanish_es.php

---
+++ lang/spanish_es.php
@@ -0,0 +1,133 @@
+<?php
+//
+// Spanish ES Language Pack
+// Blas Cantero
+// 27-Sept-2017
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Voz Digital",
+  "configuration"               =>  "Configuracion",
+  "dashboard_for"               =>  "Tablero para",
+  // Banner links
+  "dashboard"                   =>  "Tablero",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Encendido",
+  "update"                      =>  "Actualizar",
+  "backup_restore"              =>  "Restaurar copia de seguridad",
+  "factory_reset"               =>  "Restaurar datos de fabrica",
+  "live_logs"                   =>  "Informes-Logs",
+  // Config page section headdings
+  "hardware_info"               =>  "Informacion de hardware",
+  "control_software"            =>  "Control de software",
+  "mmdvmhost_config"            =>  "MMDVMHost Configuracion",
+  "general_config"              =>  "Configuracion General",
+  "dmr_config"                  =>  "DMR Configuracion",
+  "dstar_config"                =>  "D-Star Configuracion",
+  "ysf_config"                  =>  "Yaesu C4FM Configuracion fusion",
+  "p25_config"                  =>  "P25 Configuracion",
+  "wifi_config"                 =>  "Configuracion WIFI",
+  "fw_config"                   =>  "Configuracion cortafuegos",
+  "remote_access_pw"            =>  "Contraseña accceso Remoto",
+  // Config Page - Section General
+  "setting"                     =>  "Ajustes",
+  "value"                       =>  "Valor",
+  "apply"                       =>  "Aplicar",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostname",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Plataforma",
+  "cpu_load"                    =>  "CPU Rendimiento",
+  "cpu_temp"                    =>  "CPU Temperatura",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controlador Software",
+  "controller_mode"             =>  "Controlador de Modo",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "Modo DMR",
+  "d-star_mode"                 =>  "Modo D-Star",
+  "ysf_mode"                    =>  "Modo YSF",
+  "p25_mode"                    =>  "Modo P25",
+  "mmdvm_display"               =>  "MMDVM Tipo Display",
+  "mode_hangtime"               =>  "Modo tiempo de suspension",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Nodo indicativo de llamada",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Frecuencia RX/TX",
+  "lattitude"                   =>  "Latitud",
+  "longitude"                   =>  "Longitud",
+  "town"                        =>  "Ciudad",
+  "country"                     =>  "Pais",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Tipo modem",
+  "node_type"                   =>  "Nodo Tipo",
+  "timezone"                    =>  "Sistema zona horaria",
+  "dash_lang"                   =>  "Tablero de idiomas",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister red",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ red",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master habilitar",
+  "dmr_cc"                      =>  "DMR Codigo de color",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 indicativo de llamada",
+  "dstar_rpt2"                  =>  "RPT2 indicativo de llamada",
+  "dstar_irc_password"          =>  "ircDDBGateway contrasena",
+  "dstar_default_ref"           =>  "Reflector predeterminado",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "ircDDBGateway Language",
+  "dstar_irc_time"              =>  "Tiempo Anuncios",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Lanzamiento Host",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Lanzamiento Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Tablero de acceso",
+  "fw_irc"                      =>  "ircDDGBateway Remoto",
+  "fw_ssh"                      =>  "SSH Acceso",
+  // Config Page - Password
+  "user"                        =>  "Nombre de usuario",
+  "password"                    =>  "Contrasena",
+  "set_password"                =>  "Set contrasena",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Modos habilitado",
+  "net_status"                  =>  "Estatus de red",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Informacion de Radio",
+  "dstar_repeater"              =>  "D-Star Repetidor",
+  "dstar_net"                   =>  "D-Star red",
+  "dmr_repeater"                =>  "DMR Repetidor",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF red",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 red",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Time",
+  "mode"                        =>  "Modo",
+  "callsign"                    =>  "Indicativo de llamada",
+  "target"                      =>  "Objetivo",
+  "src"                         =>  "SRC",        //version corta de "fuente"
+  "dur"                         =>  "DUR",        //version corta de "duracion"
+  "loss"                        =>  "Perdida",
+  "ber"                         =>  "BER",        //version corta de "Error de bit"
+  // Dashboard - Extra Info
+  "group"                       =>  "Grupos",
+  "logoff"                      =>  "Finalizar",
+  "info"                        =>  "Informacion",
+  "utot"                        =>  "UTOT",       //tiempo agotado para usuario
+  "gtot"                        =>  "GTOT",       // tiempo agotado para grupo
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Ultimas 20 llamadas que accedieron al sistema",
+  "local_tx_list"               =>  "Ultimas 20 llamadas que accedieron a ese puerto",
+  "active_starnet_groups"       =>  "Grupos activos Starnet",
+  "active_starnet_members"      =>  "Miembros activos del grupo Starnet",
+  "d-star_link_manager"         =>  "D-Star gestor de enlaces",
+  "d-star_link_status"          =>  "D-Star Informacion de enlaces",
+  "service_status"              =>  "Estado del servicio"
+);
+?>
