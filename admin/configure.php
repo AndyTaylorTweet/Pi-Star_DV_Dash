c0e44a19114839f5dc3ca9001e9ab52d405dc598
Andy Taylor <andy@always-online.co.uk>
2017-12-04 20:12:57 +0000

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -133,7 +133,7 @@ $MYCALL=strtoupper($callsign);
 	  obj.style.height = numpix + 'px';
 	}
     </script>
-    <script type="text/javascript" src="/functions.js?version=1.1"></script>
+    <script type="text/javascript" src="/functions.js?version=1.2"></script>
 </head>
 <body>
 <div class="container">
