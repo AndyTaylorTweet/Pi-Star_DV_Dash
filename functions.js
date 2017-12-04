f8cf1a2596b413ea658d02503dfa5c205aa13262
Andy Taylor <andy@always-online.co.uk>
2017-12-04 20:12:28 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -8,20 +8,20 @@ function toggleField(hideObj,showObj) {
 function checkPass(){                   //used in confirm matching password entries
   var pass1 = document.getElementById('pass1');
   var pass2 = document.getElementById('pass2');
-  var message = document.getElementById('confirmMessage');
-  var button = document.getElementById('submitpwd');
+  //var message = document.getElementById('confirmMessage');
+  //var button = document.getElementById('submitpwd');
   var goodColor = "#66cc66";
   var badColor = "#ff6666";
   if((pass1.value != '') && (pass1.value == pass2.value)){
     pass2.style.backgroundColor = goodColor;
-    message.style.color = goodColor;
-    message.innerHTML = "Passwords Match!";
+    //message.style.color = goodColor;
+    //message.innerHTML = "Passwords Match!";
     //button.disabled = false;
     document.getElementById('submitpwd').removeAttribute("disabled");
  }else{
     pass2.style.backgroundColor = badColor;
-    message.style.color = badColor;
-    message.innerHTML = "Passwords Do Not Match!";
+    //message.style.color = badColor;
+    //message.innerHTML = "Passwords Do Not Match!";
     //button.disabled = true;
     document.getElementById('submitpwd').setAttribute("disabled","disabled");
   }
