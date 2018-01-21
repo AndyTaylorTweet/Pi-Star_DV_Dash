4e1d9fc06b02888df8a0dfb2ae49d457bfac671f
Andy Taylor <andy@always-online.co.uk>
2018-01-21 15:10:29 +0000

    Update repeaterinfo.php

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -129,7 +129,7 @@ $dmrMasterFile = fopen("/usr/local/etc/DMR_Hosts.txt", "r");
 $dmrMasterHost = getConfigItem("DMR Network", "Address", $mmdvmconfigs);
 if ($dmrMasterHost == '127.0.0.1') {
 	if (isset($configdmrgateway['XLX Network 1']['Address'])) { $xlxMasterHost1 = $configdmrgateway['XLX Network 1']['Address']; }
-	if (isset($configdmrgateway['XLX Network']['Address'])) { $xlxMasterHost1 = $configdmrgateway['XLX Network']['Address']; }
+	else { $xlxMasterHost1 = ""; }
 	$dmrMasterHost1 = $configdmrgateway['DMR Network 1']['Address'];
 	$dmrMasterHost2 = $configdmrgateway['DMR Network 2']['Address'];
 	while (!feof($dmrMasterFile)) {
