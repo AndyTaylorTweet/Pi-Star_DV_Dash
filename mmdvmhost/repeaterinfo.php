b9c32f80e0aba3667d64d2cb1d546d422ce5c366
Andy Taylor <andy@always-online.co.uk>
2018-01-21 23:57:31 +0000

    Update repeaterinfo.php

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -177,7 +177,7 @@ if (getEnabled("DMR Network", $mmdvmconfigs) == 1) {
                         if ((!isset($configdmrgateway['XLX Network 1']['Enabled'])) && (isset($configdmrgateway['XLX Network']['Enabled']))) {
                                 $xlxMasterHost1 = exec('grep \'XLX, Linking\|Unlinking\' /var/log/pi-star/DMRGateway-'.gmdate("Y-m-d").'.log | tail -1 | awk \'{print $5 " " $8 " " $9}\'');
 				if ( strpos($xlxMasterHost1, 'Linking') !== false ) { $xlxMasterHost1 = str_replace('Linking ', '', $xlxMasterHost1); }
-				else if ( strpos($xlxMasterHost1, 'Unlinking') !== false ) { $xlxMasterHost1 = "Not Linked"; }
+				else if ( strpos($xlxMasterHost1, 'Unlinking') !== false ) { $xlxMasterHost1 = "XLX Not Linked"; }
 				echo "<tr><td  style=\"background: #ffffff;\" colspan=\"2\">".$xlxMasterHost1."</td></tr>\n";
                         }
 			if ($configdmrgateway['DMR Network 1']['Enabled'] == 1) {
