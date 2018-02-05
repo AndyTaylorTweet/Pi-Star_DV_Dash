aee8ace935ecb10a0ad0478e87ac4efbd9daaf1b
narspt <narspt@users.noreply.github.com>
2018-02-05 10:15:11 +0000

    fix getActualLink for DMR, no priv call and show unlinked
    don't show private calls (including our own callsign...) as active links and show "not linked" after we unlink with TG 4000

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -589,6 +589,12 @@ function getActualLink($logLines, $mode) {
 					$to = trim(substr($logLine, strpos($logLine,"to") + 3));
 				}
 				if ($to !== "") {
+					if (substr($to, 0, 3) !== 'TG ') {
+						continue;
+					}
+					if ($to === "TG 4000") {
+						return "not linked";
+					}
 					if (strpos($to, ',') !== false) {
 						$to = substr($to, 0, strpos($to, ','));
 					}
@@ -608,6 +614,12 @@ function getActualLink($logLines, $mode) {
 					$to = trim(substr($logLine, strpos($logLine,"to") + 3));
 				}
 				if ($to !== "") {
+					if (substr($to, 0, 3) !== 'TG ') {
+						continue;
+					}
+					if ($to === "TG 4000") {
+						return "not linked";
+					}
 					if (strpos($to, ',') !== false) {
 						$to = substr($to, 0, strpos($to, ','));
 					}
