b17fe71b009af839ea05c079aa82ad31d5681b33
Yngwie Chou <yngwie.chou@gmail.com>
2018-02-14 19:15:09 +0800

    Display correct DMR Master Host for XLX/BM/DMR+ servers with identical IP

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -136,9 +136,9 @@ if ($dmrMasterHost == '127.0.0.1') {
 		$dmrMasterLine = fgets($dmrMasterFile);
                 $dmrMasterHostF = preg_split('/\s+/', $dmrMasterLine);
 		if ((strpos($dmrMasterHostF[0], '#') === FALSE) && ($dmrMasterHostF[0] != '')) {
-			if ($xlxMasterHost1 == $dmrMasterHostF[2]) { $xlxMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
-			if ($dmrMasterHost1 == $dmrMasterHostF[2]) { $dmrMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
-			if ($dmrMasterHost2 == $dmrMasterHostF[2]) { $dmrMasterHost2 = str_replace('_', ' ', $dmrMasterHostF[0]); }
+			if ((strpos($dmrMasterHostF[0], 'XLX_') === 0) && ($xlxMasterHost1 == $dmrMasterHostF[2])) { $xlxMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
+			if ((strpos($dmrMasterHostF[0], 'BM_') === 0) && ($dmrMasterHost1 == $dmrMasterHostF[2])) { $dmrMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
+			if ((strpos($dmrMasterHostF[0], 'DMR+_') === 0) && ($dmrMasterHost2 == $dmrMasterHostF[2])) { $dmrMasterHost2 = str_replace('_', ' ', $dmrMasterHostF[0]); }
 		}
 	}
 	if (strlen($xlxMasterHost1) > 21) { $xlxMasterHost1 = substr($xlxMasterHost1, 0, 19) . '..'; }
