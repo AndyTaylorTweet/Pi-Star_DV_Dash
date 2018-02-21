34258e5344ed0ea3356ca2bf077e624e4b147e7d
Andy Taylor <andy@always-online.co.uk>
2018-02-21 18:17:40 +0000

    Create catalan_es.php

---
+++ lang/catalan_es.php
@@ -0,0 +1,133 @@
+<?php
+//
+// Catalan ES Language Pack
+// Rafael EA3BIL
+// 21-Feb-2018
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Veu Digital",
+  "configuration"               =>  "Configuracio",
+  "dashboard_for"               =>  "Panell de control per",
+  // Banner links
+  "dashboard"                   =>  "Panell de Control",
+  "admin"                       =>  "Administrar",
+  "power"                       =>  "Reiniciar/Apagar",
+  "update"                      =>  "Actualitzar",
+  "backup_restore"              =>  "Backup/Restaurar copia de seguridad",
+  "factory_reset"               =>  "Restaurar dades de fabrica",
+  "live_logs"                   =>  "Informes-Logs",
+  // Config page section headdings
+  "hardware_info"               =>  "Informacio del hardware",
+  "control_software"            =>  "Control de software",
+  "mmdvmhost_config"            =>  "Configuracio de MMDVMHost",
+  "general_config"              =>  "Configuracio General",
+  "dmr_config"                  =>  "Configuracio de DMR",
+  "dstar_config"                =>  "Configuracio de DSTAR",
+  "ysf_config"                  =>  "Configuracio de Yaesu C4FM fusion",
+  "p25_config"                  =>  "Configuracio de P25",
+  "wifi_config"                 =>  "Configuracio WIFI",
+  "fw_config"                   =>  "Configuracio del tallafocs",
+  "remote_access_pw"            =>  "Mot de pas d'accces Remot",
+  // Config Page - Section General
+  "setting"                     =>  "Adjustus",
+  "value"                       =>  "Valor",
+  "apply"                       =>  "Aplicar",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostname",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Plataforma",
+  "cpu_load"                    =>  "Carrega de CPU",
+  "cpu_temp"                    =>  "Temperatura CPU",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controlador Software",
+  "controller_mode"             =>  "Controlador de Mode",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "Mode DMR",
+  "d-star_mode"                 =>  "Mode D-Star",
+  "ysf_mode"                    =>  "Mode YSF",
+  "p25_mode"                    =>  "Mode P25",
+  "mmdvm_display"               =>  "MMDVM Tipus Display",
+  "mode_hangtime"               =>  "Modo temps de suspensio",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Indicatiu de Node",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Frequencia RX/TX",
+  "lattitude"                   =>  "Latitud",
+  "longitude"                   =>  "Longitud",
+  "town"                        =>  "Ciutat",
+  "country"                     =>  "Pais",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Tipus modem",
+  "node_type"                   =>  "Node Tipus",
+  "timezone"                    =>  "Zona horaria",
+  "dash_lang"                   =>  "Idioma del Panell de Control",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "Master de BrandMeister",
+  "bm_network"                  =>  "Xarxa de BrandMeister",
+  "dmr_plus_master"             =>  "Master de DMR+",
+  "dmr_plus_network"            =>  "Xarxa de DMR+",
+  "xlx_master"                  =>  "Master de XLX",
+  "xlx_enable"                  =>  "habilitar Master XLX",
+  "dmr_cc"                      =>  "Codi de color de DMR",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 indicatiu de crida",
+  "dstar_rpt2"                  =>  "RPT2 indicatiu de crida",
+  "dstar_irc_password"          =>  "mot de pas de ircDDBGateway",
+  "dstar_default_ref"           =>  "Reflector predeterminat",
+  "aprs_host"                   =>  "Servidor de APRS",
+  "dstar_irc_lang"              =>  "Idioma d'ircDDBGateway",
+  "dstar_irc_time"              =>  "Interval de Balius",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Arrencant Host",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Arrencant Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Taulell d'acces",
+  "fw_irc"                      =>  "ircDDGBateway Remoto",
+  "fw_ssh"                      =>  "Acces per SSH",
+  // Config Page - Password
+  "user"                        =>  "Nom d'usuario",
+  "password"                    =>  "Mot de pas",
+  "set_password"                =>  "Establir mot de pas",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Modes habilitats",
+  "net_status"                  =>  "Estat de xarxa",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Informacio de Radio",
+  "dstar_repeater"              =>  "Repetidor de D-Star",
+  "dstar_net"                   =>  "Xarxa de D-Star",
+  "dmr_repeater"                =>  "Repetidor de DMR",
+  "dmr_master"                  =>  "Master de DMR",
+  "ysf_net"                     =>  "Xarxa de YSF",
+  "p25_radio"                   =>  "Radio de P25",
+  "p25_net"                     =>  "Xarxa de P25",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Hora",
+  "mode"                        =>  "Mode",
+  "callsign"                    =>  "Indicatiu de crida",
+  "target"                      =>  "Desti",
+  "src"                         =>  "SRC",        //version corta de "fuente"
+  "dur"                         =>  "DUR",        //version corta de "duracion"
+  "loss"                        =>  "Perdua",
+  "ber"                         =>  "BER",        //version corta de "Error de bit"
+  // Dashboard - Extra Info
+  "group"                       =>  "Grups",
+  "logoff"                      =>  "Finalitzar",
+  "info"                        =>  "Informacio",
+  "utot"                        =>  "UTOT",       //tiempo agotado para usuario
+  "gtot"                        =>  "GTOT",       // tiempo agotado para grupo
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Ultimes 20 trucadas que han accedit al sistema",
+  "local_tx_list"               =>  "Ultimes 20 trucadas que han accedit al port",
+  "active_starnet_groups"       =>  "Grups actius Starnet",
+  "active_starnet_members"      =>  "Membres actius del grup Starnet",
+  "d-star_link_manager"         =>  "Gestor d'enllaços D-Star",
+  "d-star_link_status"          =>  "Informacio d'enllaços D-Star",
+  "service_status"              =>  "Estat del servei"
+);
+?>
