4bdff9c62aaac79d3977bf6f52558b26b689057d
Andy Taylor <andy@always-online.co.uk>
2018-02-23 12:10:01 +0000

    Update japanese_jp.php

--- lang/japanese_jp.php
+++ lang/japanese_jp.php
@@ -88,6 +88,9 @@ $lang = array (
   // Config Page - P25 Configuration
   "p25_startup_host"            =>  "P25 スタートアップ　ホスト",
   "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN スタートアップ　ホスト",
+  "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
   "fw_dash"                     =>  "ダッシュボード　アクセス",
   "fw_irc"                      =>  "ircDDGBateway リモート",
