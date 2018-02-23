a3b7294c04883e6af26d12a86db97f9475ff24ee
Andy Taylor <andy@always-online.co.uk>
2018-02-23 13:15:53 +0000

    Update norwegian_no.php

--- lang/norwegian_no.php
+++ lang/norwegian_no.php
@@ -111,6 +111,8 @@ $lang = array (
   "ysf_net"                     =>  "YSF Nettverk",
   "p25_radio"                   =>  "P25 Radio",
   "p25_net"                     =>  "P25 Nettverk",
+  "nxdn_radio"                  =>  "NXDN Radio",
+  "nxdn_net"                    =>  "NXDN Nettverk",
   // Dashboard Front Page - Calls
   "time"                        =>  "Tid",
   "mode"                        =>  "Mode",
