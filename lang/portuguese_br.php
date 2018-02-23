949ae1cb81fe815edea994e23ce6f48524e88977
Andy Taylor <andy@always-online.co.uk>
2018-02-23 12:11:00 +0000

    Update portuguese_br.php

--- lang/portuguese_br.php
+++ lang/portuguese_br.php
@@ -88,6 +88,9 @@ $lang = array (
   // Config Page - P25 Configuration
   "p25_startup_host"            =>  "Servidor Inicial P25",
   "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "Servidor Inicial NXDN",
+  "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
   "fw_dash"                     =>  "Acesso ao Painel",
   "fw_irc"                      =>  "Remoto ircDDGBateway",
