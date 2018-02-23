891df8bea334cd1188342159871fb4081393d16c
Andy Taylor <andy@always-online.co.uk>
2018-02-23 12:08:36 +0000

    Update german_de.php

--- lang/german_de.php
+++ lang/german_de.php
@@ -88,6 +88,9 @@ $lang = array (
   // Config Page - P25 Configuration
   "p25_startup_host"            =>  "P25 Startup Host",
   "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN Startup Host",
+  "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
   "fw_dash"                     =>  "Tableau Zugriff",
   "fw_irc"                      =>  "ircDDGBateway Remote",
