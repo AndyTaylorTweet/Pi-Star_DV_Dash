f06c19f6a9ebe15e27d6cc8950e499902465f00e
Andy Taylor <andy@always-online.co.uk>
2018-02-23 12:07:01 +0000

    Update dutch_nl.php

--- lang/dutch_nl.php
+++ lang/dutch_nl.php
@@ -87,6 +87,9 @@ $lang = array (
   // Config Page - P25 Configuration
   "p25_startup_host"            =>  "P25 standaard Host",
   "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN standaard Host",
+  "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
   "fw_dash"                     =>  "Dashboard toegang",
   "fw_irc"                      =>  "ircDDGBateway op afstand",
