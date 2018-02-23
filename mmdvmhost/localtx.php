1f3d767d5b60a0528b67870c1387873ca48d8e3e
narspt <narspt@users.noreply.github.com>
2018-02-23 05:48:00 +0000

    localtx.php: add rssi information

--- mmdvmhost/localtx.php
+++ mmdvmhost/localtx.php
@@ -16,7 +16,8 @@ $localTXList = $lastHeard;
       <th><a class="tooltip" href="#"><?php echo $lang['target'];?><span><b>Target, D-Star Reflector, DMR Talk Group etc</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['src'];?><span><b>Recieved from source</b></span></a></th>
       <th><a class="tooltip" href="#"><?php echo $lang['dur'];?>(s)<span><b>Duration in Seconds</b></span></a></th>
-      <th><a class="tooltip" href="#"><?php echo $lang['ber'];?><span><b>Bit Error Rate</b></span></a></th>
+      <th style="min-width:5ch"><a class="tooltip" href="#"><?php echo $lang['ber'];?><span><b>Bit Error Rate</b></span></a></th>
+      <th style="min-width:8ch"><a class="tooltip" href="#">RSSI<span><b>Received Signal Strength Indication</b></span></a></th>
     </tr>
 <?php
 $counter = 0;
@@ -46,17 +47,19 @@ for ($i = 0; $i < count($localTXList); $i++) {
 				echo "<td>$listElem[5]</td>";
 			}
 			if ($listElem[6] == null) {
-				echo "<td style=\"background:#f33;\">TX</td><td></td>";
+				echo "<td style=\"background:#f33;\">TX</td><td></td><td></td>";
 			} else if ($listElem[6] == "SMS") {
-				echo "<td>SMS</td><td></td>";
+				echo "<td>SMS</td><td></td><td></td>";
 			} else {
-				echo"<td>$listElem[6]</td>";
+				echo"<td>$listElem[6]</td>"; //duration
 				
 				// Colour the BER Field
 				if (floatval($listElem[8]) == 0) { echo "<td>$listElem[8]</td>"; }
 				elseif (floatval($listElem[8]) >= 0.0 && floatval($listElem[8]) <= 1.9) { echo "<td style=\"background:#1d1;\">$listElem[8]</td>"; }
 				elseif (floatval($listElem[8]) >= 2.0 && floatval($listElem[8]) <= 4.9) { echo "<td style=\"background:#fa0;\">$listElem[8]</td>"; }
 				else { echo "<td style=\"background:#f33;\">$listElem[8]</td>"; }
+
+				echo"<td>$listElem[9]</td>"; //rssi
 			}
 			echo"</tr>\n";
 			$counter++; }
