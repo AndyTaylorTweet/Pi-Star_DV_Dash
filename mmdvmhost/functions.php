5673dd424da3fdb2d2f0a445f06d0e0c341cb302
Andy Taylor <andy@always-online.co.uk>
2018-02-24 09:01:07 +0000

    Update functions.php

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -814,5 +814,4 @@ $P25Gatewayconfigs = getP25GatewayConfig();
 $logLinesP25Gateway = getP25GatewayLog();
 $reverseLogLinesP25Gateway = $logLinesP25Gateway;
 array_multisort($reverseLogLinesP25Gateway,SORT_DESC);
-$ysf2dmrconfigs = getYSF2DMRConfig();
 ?>
