1e05afc0a171e6bc705a550455b6fec9490954f6
Andy Taylor <andy@always-online.co.uk>
2018-03-02 18:34:38 +0000

    Update edit_bmapikey.php

--- admin/expert/edit_bmapikey.php
+++ admin/expert/edit_bmapikey.php
@@ -126,13 +126,13 @@ echo '<form action="" method="post">'."\n";
 		// note the name='' attribute it has both section and key
 		foreach($values as $key=>$value) {
 			if (($key == "Options") || ($value)) {
-				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"40\" rows=\"3\">$value</textarea></td></tr>\n";
+				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"60\" rows=\"3\">$value</textarea></td></tr>\n";
 			}
 			elseif (($key == "Display") && ($value == '')) {
-				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"40\" rows=\"3\">$value</textarea></td></tr>\n";
+				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"60\" rows=\"3\">$value</textarea></td></tr>\n";
 			}
 			else {
-				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"40\" rows=\"3\">$value</textarea></td></tr>\n";			
+				echo "<tr><td align=\"right\" width=\"30%\">$key</td><td align=\"left\"><textarea name=\"{$section}[$key]\" cols=\"60\" rows=\"3\">$value</textarea></td></tr>\n";			
 			}
 		}
 		echo "</table>\n";
