8d1de2733beba9c36a39cc01a55659819e37f2e8
Andy Taylor <andy@always-online.co.uk>
2018-03-02 11:41:07 +0000

    Update bm_links.php

--- mmdvmhost/bm_links.php
+++ mmdvmhost/bm_links.php
@@ -113,17 +113,17 @@ if ( $testMMDVModeDMR == 1 ) {
       'timeslot' => $targetSlot
     );
 
-    $postdata = http_build_query(
-      'talkgroup' => $targetTG,
-      'timeslot' => $targetSlot
-    );
+    //$postdata = http_build_query(
+    //  'talkgroup' => $targetTG,
+    //  'timeslot' => $targetSlot
+    //);
 
     $opts = array('http' =>
       array(
         'method'  => 'POST',
         'header'  => $postHeaders,
-        //'data' => json_encode($jsonData)
-        'content' => $postdata
+        'data' => json_encode($jsonData)
+        //'content' => $postdata
       )
     );
 
