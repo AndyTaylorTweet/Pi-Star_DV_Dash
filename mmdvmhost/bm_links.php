e785741b202ea5653d10ad1fcf78c6b5b6ef98b6
Andy Taylor <andy@always-online.co.uk>
2018-03-02 14:32:33 +0000

    Update bm_links.php

--- mmdvmhost/bm_links.php
+++ mmdvmhost/bm_links.php
@@ -39,7 +39,7 @@ if ( $testMMDVModeDMR == 1 ) {
   $dmrID = getConfigItem("General", "Id", $mmdvmconfigs);
 
   // Use BM API to get information about current TGs
-  $json = json_decode(file_get_contents("https://api.brandmeister.network/v1.0/repeater/?action=profile&q=$dmrID", true));
+  $json = json_decode(file_get_contents("https://api.brandmeister.network/v1.0/repeater/?action=PROFILE&q=$dmrID", true));
 
   // Set some Variable
   $bmStaticTGList = "";
