d0ce084326d8fabe08f689762af8a2d2d7ca85e7
Andy Taylor <andy@always-online.co.uk>
2018-03-08 19:30:28 +0000

    Update functions.php

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -493,7 +493,7 @@ function getHeardList($logLines) {
 				$target		= preg_replace('!\s+!', ' ', $target);
                 		break;
 			case "P25":
-				if ($source == "Net" && $target == "TG 10") {$target = $callsign; $callsign = "P25Parrot";}
+				if ($source == "Net" && $target == "TG 10") {$callsign = "10";}
                 		$duration	= $p25duration;
                 		$loss		= $p25loss;
                 		$ber		= $p25ber;
