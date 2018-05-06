ff2ae8eb16d692463a11e12f7bf122aa19bbdcb4
Andy Taylor <andy@always-online.co.uk>
2018-05-06 19:36:25 +0100

    Update functions.php

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -149,6 +149,13 @@ function showMode($mode, $mmdvmconfigs) {
 			echo "<td style=\"background:#606060; color:#b0b0b0;\">";
 		}
 	}
+	elseif ( ($mode == "YSF2P25 Network") && (getEnabled("System Fusion", $mmdvmconfigs) == 1) ) {
+		if (isProcessRunning("YSF2P25")) {
+			echo "<td style=\"background:#0b0; color:#030; width:50%;\">";
+		} else {
+			echo "<td style=\"background:#606060; color:#b0b0b0;\">";
+		}
+	}
 	else {
 		echo "<td style=\"background:#606060; color:#b0b0b0;\">";
     }
