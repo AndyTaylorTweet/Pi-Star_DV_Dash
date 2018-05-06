203feab9795c808ae44a968541d9dd14f6e73d28
Andy Taylor <andy@always-online.co.uk>
2018-05-06 19:00:14 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -659,7 +659,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	  }
 
 	  $configmmdvm['General']['Callsign'] = $newCallsignUpper;
-	  $configysfgateway['General']['Callsign'] = $newCallsignUpper;
+	  $configysfgateway['General']['Callsign'] = str_pad($newCallsignUpper,  6, "-100");
 	  $configysfgateway['aprs.fi']['Password'] = aprspass($newCallsignUpper);
 	  $configysfgateway['aprs.fi']['Description'] = $newCallsignUpper."_Pi-Star";
 	  $configysf2dmr['aprs.fi']['Password'] = aprspass($newCallsignUpper);
