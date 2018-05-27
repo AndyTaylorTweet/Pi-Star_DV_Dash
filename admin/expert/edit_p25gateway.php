0b0871842f9208d59ac8009ba5b4437e2efcb060
Andy Taylor <andy@always-online.co.uk>
2018-05-27 10:31:17 +0100

    Update edit_p25gateway.php

--- admin/expert/edit_p25gateway.php
+++ admin/expert/edit_p25gateway.php
@@ -24,7 +24,7 @@ require_once('../config/version.php');
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <meta http-equiv="Expires" content="0" />
     <title>Pi-Star - Digital Voice Dashboard - Expert Editor</title>
-    <link rel="stylesheet" type="text/css" href="../css/ircddb.css?version=1.3" />
+    <link rel="stylesheet" type="text/css" href="../css/pistar-css.php" />
   </head>
   <body>
   <div class="container">
