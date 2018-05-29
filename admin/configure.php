00c9cb8584712e575c65ac2deae055fb1b32f79f
Andy Taylor <andy@always-online.co.uk>
2018-05-29 21:53:52 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -1380,12 +1380,12 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 
 	// Set DMR2NXDN Mode
 	if (empty($_POST['MMDVMModeDMR2NXDN']) != TRUE ) {
-	  if (empty($_POST['MMDVMModeDMR2YSF']) != TRUE ) {
-		  if (escapeshellcmd($_POST['MMDVMModeDMR2YSF']) == 'ON' )  {
-			  $configdmr2ysf['Enabled']['Enabled'] = "0";
-		  }
-	  }
           if (escapeshellcmd($_POST['MMDVMModeDMR2NXDN']) == 'ON' )  {
+		  if (empty($_POST['MMDVMModeDMR2YSF']) != TRUE ) {
+		  	if (escapeshellcmd($_POST['MMDVMModeDMR2YSF']) == 'ON' )  {
+				$configdmr2ysf['Enabled']['Enabled'] = "0";
+		  	}
+	  	  }
 		  $configdmr2nxdn['Enabled']['Enabled'] = "1";
 		  unset($configdmrgateway['DMR Network 3']);
 		  $configdmrgateway['DMR Network 3']['Enabled'] = "0";
