a2f9df16e58879b10b7c34184a7ccb7ee7a0c633
Andy Taylor <andy@always-online.co.uk>
2018-05-30 18:38:37 +0100

    Update repeaterinfo.php

--- mmdvmhost/repeaterinfo.php
+++ mmdvmhost/repeaterinfo.php
@@ -166,7 +166,7 @@ if ($dmrMasterHost == '127.0.0.1') {
 	else { $xlxMasterHost1 = ""; }
 	$dmrMasterHost1 = $configdmrgateway['DMR Network 1']['Address'];
 	$dmrMasterHost2 = $configdmrgateway['DMR Network 2']['Address'];
-	$dmrMasterHost3 = $configdmrgateway['DMR Network 3']['Address'];
+	$dmrMasterHost3 = str_replace('_', ' ', $configdmrgateway['DMR Network 3']['Name']);
 	while (!feof($dmrMasterFile)) {
 		$dmrMasterLine = fgets($dmrMasterFile);
                 $dmrMasterHostF = preg_split('/\s+/', $dmrMasterLine);
@@ -174,7 +174,6 @@ if ($dmrMasterHost == '127.0.0.1') {
 			if ((strpos($dmrMasterHostF[0], 'XLX_') === 0) && ($xlxMasterHost1 == $dmrMasterHostF[2])) { $xlxMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
 			if ((strpos($dmrMasterHostF[0], 'BM_') === 0) && ($dmrMasterHost1 == $dmrMasterHostF[2])) { $dmrMasterHost1 = str_replace('_', ' ', $dmrMasterHostF[0]); }
 			if ((strpos($dmrMasterHostF[0], 'DMR+_') === 0) && ($dmrMasterHost2 == $dmrMasterHostF[2])) { $dmrMasterHost2 = str_replace('_', ' ', $dmrMasterHostF[0]); }
-			if ((strpos($dmrMasterHostF[0], 'DMR2') === 0) && ($dmrMasterHost3 == $dmrMasterHostF[2])) { $dmrMasterHost3 = str_replace('_', ' ', $dmrMasterHostF[0]); }
 		}
 	}
 	if (strlen($xlxMasterHost1) > 21) { $xlxMasterHost1 = substr($xlxMasterHost1, 0, 19) . '..'; }
