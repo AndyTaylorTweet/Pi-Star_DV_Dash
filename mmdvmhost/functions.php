6910fb6020482d37edf8540c3783722fec154473
Andy Taylor <andy@always-online.co.uk>
2018-06-01 11:30:33 +0100

    Update functions.php

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -241,8 +241,8 @@ function getP25GatewayLog() {
 		$logPath1 = P25GATEWAYLOGPATH."/".P25GATEWAYLOGPREFIX."-".gmdate("Y-m-d").".log";
 		$logLines1 = explode("\n", `egrep -h "ink" $logPath1 | cut -d" " -f2- | tail -5`);
         }
-	$logLines1 = array_slice($logLines1, -25);
-        if (sizeof($logLines1) < 25) {
+	$logLines1 = array_slice($logLines1, -5);
+        if (sizeof($logLines1) < 5) {
                 if (file_exists(P25GATEWAYLOGPATH."/".P25GATEWAYLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log")) {
                         //if ($log = fopen(P25GATEWAYLOGPATH."/".P25GATEWAYLOGPREFIX."-".gmdate("Y-m-d", time() - 86340).".log", 'r')) {
                         //        while ($logLine = fgets($log)) {
@@ -256,9 +256,11 @@ function getP25GatewayLog() {
 			$logLines2 = explode("\n", `egrep -h "ink" $logPath2 | cut -d" " -f2- | tail -5`);
                 }
         }
-	$logLines2 = array_slice($logLines2, -25);
-	$logLines = $logLines1 + $logLines2;
-	$logLines = array_slice($logLines, -25);
+	$logLines2 = array_slice($logLines2, -5);
+	$logLines1 = array_slice($logLines1, -5);
+	if (sizeof($logLines1) < 5) { $logLines = $logLines2 + $logLines1; } else { $logLines = $logLines1; }
+	//$logLines = $logLines1 + $logLines2;
+	$logLines = array_slice($logLines, -5);
         return $logLines;
 }
 
