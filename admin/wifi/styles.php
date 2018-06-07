fbb2f93f88fc27b9c272c1f04441c7cf1138dd9a
Andy Taylor <andy@always-online.co.uk>
2018-06-07 09:55:52 +0100

    Create styles.php

---
+++ admin/wifi/styles.php
@@ -0,0 +1,100 @@
+body {
+	background-color: #dd4b39;
+}
+
+.red {
+	color:red;
+}
+
+.green {
+	color:green;
+}
+
+.network {
+        border:1px solid;
+	text-align:left;
+        padding-top:5px;
+        padding-left:5px;
+	background-color: #d0d0d0;
+}
+
+.tableft {
+	display:inline-block;
+	text-align:right;
+	width:120px;
+}
+
+textarea {
+        width:699px;
+        height:499px;
+}
+
+input.button {
+        width:80%;
+        border:1px solid;
+}
+
+input[type=text],input[type=password] {
+	border:1px solid;
+}
+
+input[type=button],input[type=submit] {
+	border:1px solid;
+	border-radius:5px;
+}
+
+.infoheader {
+	width:100%;
+	text-align:center;
+	margin-top:10px;
+	border-bottom: 1px solid;
+}
+
+.infobox {
+	background-color: #dd4b39;
+	font-weight: bold;
+	width:100%;
+}
+
+.intinfo {
+	background-color: #d0d0d0;
+	color: #000000;
+	width: 398px;
+	text-align: left;
+	border-right: 1px solid;
+	border-left: 1px solid;
+	float: left;
+        border-bottom:1px solid;
+}
+
+.wifiinfo {
+	background-color: #d0d0d0;
+	color: #000000;
+	margin: 0 0 0 400px;
+	border-right: 1px solid;
+	text-align: left;
+        border-bottom:1px solid;
+}
+
+.intheader {
+	background-color: #dd4b39;
+	color: #ffffff;
+	text-align:center;
+	width:100%;
+	font-weight: bold;
+}
+
+.intfooter {
+	background-color: #dd4b39;
+	color: #ffffff;
+	width:100%;
+	border-top:1px solid;
+	float:left;
+	text-align:center;
+}
+
+.tail {
+	background-color: #dd4b39;
+	color: #dd4b39;
+	width:100%;
+}
