e685cfcad5742136f2c47a5912b9767922ac1560
Andy Taylor <andy@always-online.co.uk>
2018-06-18 20:28:33 +0100

    Create download_modem_log.php

---
+++ admin/download_modem_log.php
@@ -0,0 +1,26 @@
+<?php
+
+if (file_exists('/etc/dstar-radio.mmdvmhost')) {
+$logfile = "/var/log/pi-star/MMDVM-".gmdate('Y-m-d').".log";
+}
+elseif (file_exists('/etc/dstar-radio.dstarrepeater')) {
+$logfile = "/var/log/pi-star/DStarRepeater-".gmdate('Y-m-d').".log";
+}
+
+$unixfile = file_get_contents($logfile);
+$dosfile = str_replace("\n", "\r\n", $unixfile);
+
+header('Pragma: public');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Cache-Control: private', false);
+header('Content-Type: text/plain');
+
+header('Content-Disposition: attachment; filename="'. basename($logfile) . '";');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($dosfile));
+
+readfile($dosfile);
+
+exit;
+?>
