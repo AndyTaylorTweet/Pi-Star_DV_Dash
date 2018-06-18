9c607840dc9e65bf8d2c381e2412a35caab28709
Andy Taylor <andy@always-online.co.uk>
2018-06-18 20:58:44 +0100

    Update download_modem_log.php

--- admin/download_modem_log.php
+++ admin/download_modem_log.php
@@ -1,41 +1,44 @@
 <?php
-if (file_exists('/etc/dstar-radio.mmdvmhost')) {
-	$logfile = "/var/log/pi-star/MMDVM-".gmdate('Y-m-d').".log";
+if ($_SERVER["PHP_SELF"] == "/admin/live_modem_log.php") {
+	if (file_exists('/etc/dstar-radio.mmdvmhost')) {
+		$logfile = "/var/log/pi-star/MMDVM-".gmdate('Y-m-d').".log";
+	}
+	elseif (file_exists('/etc/dstar-radio.dstarrepeater')) {
+		$logfile = "/var/log/pi-star/DStarRepeater-".gmdate('Y-m-d').".log";
+	}
+
+	$unixfile = file_get_contents($logfile);
+	$dosfile = str_replace("\n", "\r\n", $unixfile);
+
+	header('Pragma: public');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	header('Cache-Control: private', false);
+	header('Content-Type: text/plain');
+	header('Content-Disposition: attachment; filename="Pi-Star_'.basename($logfile).'";');
+	header('Content-Length: '.filesize($logfile));
+	header('Accept-Ranges: bytes');
+
+	// User Agent Detection
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'indows') !== false) {
+		$userAgent = "Windows";
+	} else {
+		$userAgent = "NonWindows";
+	}
+
+	// Pre-flight checks done, send the output.
+	set_time_limit(0);
+	$file = @fopen($logfile,"rb");
+	while(!feof($file)) {
+		if ($userAgent == "Windows") { print(str_replace("\n", "\r\n", @fread($file, 1024*8))); }
+		if ($userAgent == "NonWindows") { print(@fread($file, 1024*8)); }
+		ob_flush();
+		flush();
+	}
+
+	// Ok we are done, close the file and clean up.
+	@fclose($file);
+	exit;
 }
-elseif (file_exists('/etc/dstar-radio.dstarrepeater')) {
-	$logfile = "/var/log/pi-star/DStarRepeater-".gmdate('Y-m-d').".log";
-}
-
-$unixfile = file_get_contents($logfile);
-$dosfile = str_replace("\n", "\r\n", $unixfile);
-
-header('Pragma: public');
-header('Expires: 0');
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-header('Cache-Control: private', false);
-header('Content-Type: text/plain');
-header('Content-Disposition: attachment; filename="Pi-Star_'.basename($logfile).'";');
-header('Content-Length: '.filesize($logfile));
-header('Accept-Ranges: bytes');
-
-// User Agent Detection
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'indows') !== false) {
-	$userAgent = "Windows";
-} else {
-	$userAgent = "NonWindows";
-}
-
-// Pre-flight checks done, send the output.
-set_time_limit(0);
-$file = @fopen($logfile,"rb");
-while(!feof($file)) {
-	if ($userAgent == "Windows") { print(str_replace("\n", "\r\n", @fread($file, 1024*8))); }
-	if ($userAgent == "NonWindows") { print(@fread($file, 1024*8)); }
-	ob_flush();
-	flush();
-}
-
-// Ok we are done, close the file and clean up.
-@fclose($file);
-exit;
+else { die; }
 ?>
