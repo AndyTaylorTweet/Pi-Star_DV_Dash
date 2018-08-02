c893a5acb88c148059470e3ad551ea8961772469
Andy Taylor <andy@always-online.co.uk>
2018-08-02 22:18:19 +0100

    Update japanese_jp.php

--- lang/japanese_jp.php
+++ lang/japanese_jp.php
@@ -94,7 +94,7 @@ $lang = array (
   "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
   "fw_dash"                     =>  "ダッシュボード　アクセス",
-  "fw_irc"                      =>  "ircDDGBateway リモート",
+  "fw_irc"                      =>  "ircDDBGateway リモート",
   "fw_ssh"                      =>  "SSH アクセス",
   // Config Page - Password
   "user"                        =>  "ユーザー名",
