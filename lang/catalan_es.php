efb2f5ad1e56c9d7ffde08317b759167e1dc6997
Franc Rodriguez <franc@tecob.com>
2018-10-15 13:25:37 +0200

    Catalan Language: Ortographical fixes

--- lang/catalan_es.php
+++ lang/catalan_es.php
@@ -3,34 +3,35 @@
 // Catalan ES Language Pack
 // Rafa EA3BIL
 // 08-Mar-2018. Some updates done after linguistic expert review.
+// 15-Oct-2018 . Ortographical fixes by C31FR - Franc
 //
 $lang = array (
   // Banner texts
   "digital_voice"               =>  "Veu Digital",
-  "configuration"               =>  "Configuracio",
-  "dashboard_for"               =>  "Panell de control per",
+  "configuration"               =>  "Configuració",
+  "dashboard_for"               =>  "Panell de control de",
   // Banner links
   "dashboard"                   =>  "Panell de Control",
   "admin"                       =>  "Administrar",
   "power"                       =>  "Reiniciar/Apagar",
   "update"                      =>  "Actualitzar",
   "backup_restore"              =>  "Fer/Restaurar copia seguretat",
-  "factory_reset"               =>  "Restaurar estat de fabrica",
+  "factory_reset"               =>  "Restaurar estat de fàbrica",
   "live_logs"                   =>  "Informes-Logs",
   // Config page section headdings
-  "hardware_info"               =>  "Informacio del hardware",
-  "control_software"            =>  "Control de software",
-  "mmdvmhost_config"            =>  "Configuracio de MMDVMHost",
-  "general_config"              =>  "Configuracio General",
-  "dmr_config"                  =>  "Configuracio de DMR",
-  "dstar_config"                =>  "Configuracio de DSTAR",
-  "ysf_config"                  =>  "Configuracio de Yaesu C4FM fusion",
-  "p25_config"                  =>  "Configuracio de P25",
-  "nxdn_config"                 =>  "Configuracio de NXDN",
-  "pocsag_config"               =>  "Configuracio de POCSAG",
-  "wifi_config"                 =>  "Configuracio WIFI",
-  "fw_config"                   =>  "Configuracio del Tallafocs",
-  "remote_access_pw"            =>  "Password per accces Remot",
+  "hardware_info"               =>  "Informació del maquinari",
+  "control_software"            =>  "Control de programari",
+  "mmdvmhost_config"            =>  "Configuració de MMDVMHost",
+  "general_config"              =>  "Configuració General",
+  "dmr_config"                  =>  "Configuració de DMR",
+  "dstar_config"                =>  "Configuració de DSTAR",
+  "ysf_config"                  =>  "Configuració de Yaesu C4FM fusion",
+  "p25_config"                  =>  "Configuració de P25",
+  "nxdn_config"                 =>  "Configuració de NXDN",
+  "pocsag_config"               =>  "Configuració de POCSAG",
+  "wifi_config"                 =>  "Configuració WIFI",
+  "fw_config"                   =>  "Configuració del Tallafocs",
+  "remote_access_pw"            =>  "Mot clau per acccés remot",
   // Config Page - Section General
   "setting"                     =>  "Ajustaments",
   "value"                       =>  "Valor",
@@ -39,10 +40,10 @@ $lang = array (
   "hostname"                    =>  "Hostname",
   "kernel"                      =>  "Kernel",
   "platform"                    =>  "Plataforma",
-  "cpu_load"                    =>  "Carrega CPU",
+  "cpu_load"                    =>  "Càrrega CPU",
   "cpu_temp"                    =>  "Temperatura CPU",
   // Config Page - Control Software
-  "controller_software"         =>  "Controlador Software",
+  "controller_software"         =>  "Controlador programari",
   "controller_mode"             =>  "Controlador de Mode",
   // Config Page - MMDVMHost Configuration
   "dmr_mode"                    =>  "Mode DMR",
@@ -50,21 +51,21 @@ $lang = array (
   "ysf_mode"                    =>  "Mode YSF",
   "p25_mode"                    =>  "Mode P25",
   "nxdn_mode"                   =>  "Mode NXDN",
-  "mmdvm_display"               =>  "MMDVM Tipus Display",
-  "mode_hangtime"               =>  "Mode temps de suspensio",
+  "mmdvm_display"               =>  "MMDVM Tipus de pantalla",
+  "mode_hangtime"               =>  "Mode temps de suspensió",
   // Config Page - General Configuration
-  "node_call"                   =>  "Indicatiu de Node",
+  "node_call"                   =>  "Indicatiu del Node",
   "dmr_id"                      =>  "CCS7/DMR ID",
-  "radio_freq"                  =>  "Frequencia RX/TX",
-  "lattitude"                   =>  "Latitud",
-  "longitude"                   =>  "Longitud",
+  "radio_freq"                  =>  "Freqüència RX/TX",
+  "lattitude"                   =>  "Latitut",
+  "longitude"                   =>  "Longitut",
   "town"                        =>  "Ciutat",
-  "country"                     =>  "Pais",
+  "country"                     =>  "Païs",
   "url"                         =>  "URL",
-  "radio_type"                  =>  "Radio/Tipus modem",
-  "node_type"                   =>  "Node Tipus",
-  "timezone"                    =>  "Zona horaria",
-  "dash_lang"                   =>  "Idioma Panell de Control",
+  "radio_type"                  =>  "Ràdio/Tipus de modem",
+  "node_type"                   =>  "Tipus de Node",
+  "timezone"                    =>  "Zona horària",
+  "dash_lang"                   =>  "Idioma del Panell de Control",
   // Config Page - DMR Configuration
   "dmr_master"                  =>  "DMR Master (MMDVMHost)",
   "bm_master"                   =>  "Master BrandMeister",
@@ -79,7 +80,7 @@ $lang = array (
   // Config Page - D-Star Configuration
   "dstar_rpt1"                  =>  "RPT1 indicatiu de crida",
   "dstar_rpt2"                  =>  "RPT2 indicatiu de crida",
-  "dstar_irc_password"          =>  "Password d'ircDDBGateway",
+  "dstar_irc_password"          =>  "mot clau d'ircDDBGateway",
   "dstar_default_ref"           =>  "Reflector predeterminat",
   "aprs_host"                   =>  "Servidor d'APRS",
   "dstar_irc_lang"              =>  "Idioma d'ircDDBGateway",
@@ -93,18 +94,18 @@ $lang = array (
   "nxdn_startup_host"            =>  "NXDN Arrencant Host",
   "nxdn_ran"                     =>  "NXDN RAN",
   // Config Page - Firewall Configuration
-  "fw_dash"                     =>  "Panell d'acces",
+  "fw_dash"                     =>  "Panell d'accés",
   "fw_irc"                      =>  "ircDDBGateway Remot",
-  "fw_ssh"                      =>  "Acces per SSH",
+  "fw_ssh"                      =>  "Accés per SSH",
   // Config Page - Password
   "user"                        =>  "Nom d'usuari",
-  "password"                    =>  "Password",
-  "set_password"                =>  "Establir password",
+  "password"                    =>  "Mot clau",
+  "set_password"                =>  "Establir mot clau",
   // Dashboard Front Page - Repeater Info Pannel
   "modes_enabled"               =>  "Modes habilitats",
-  "net_status"                  =>  "Estat de xarxa",
+  "net_status"                  =>  "Estat de la xarxa",
   "internet"                    =>  "Internet",
-  "radio_info"                  =>  "Informacio de Radio",
+  "radio_info"                  =>  "Informació de Ràdio",
   "dstar_repeater"              =>  "Repetidor D-Star",
   "dstar_net"                   =>  "Xarxa D-Star",
   "dmr_repeater"                =>  "Repetidor DMR",
@@ -118,24 +119,24 @@ $lang = array (
   "time"                        =>  "Hora",
   "mode"                        =>  "Mode",
   "callsign"                    =>  "Indicatiu",
-  "target"                      =>  "Desti",
-  "src"                         =>  "SRC",        //version corta de "fuente"
-  "dur"                         =>  "DUR",        //version corta de "duracion"
-  "loss"                        =>  "Perdua",
-  "ber"                         =>  "BER",        //version corta de "Error de bit"
+  "target"                      =>  "Destí",
+  "src"                         =>  "SRC",  
+  "dur"                         =>  "DUR",  
+  "loss"                        =>  "Pèrdua",
+  "ber"                         =>  "BER",  
   // Dashboard - Extra Info
   "group"                       =>  "Grup",
   "logoff"                      =>  "Finalitzar",
-  "info"                        =>  "Informacio",
-  "utot"                        =>  "UTOT",       //tiempo agotado para usuario
-  "gtot"                        =>  "GTOT",       // tiempo agotado para grupo
+  "info"                        =>  "Informació",
+  "utot"                        =>  "UTOT", 
+  "gtot"                        =>  "GTOT", 
   // Dashboard Front Page / Admin - Section Headders
-  "last_heard_list"             =>  "Ultimes 20 trucades que han accedit al sistema",
-  "local_tx_list"               =>  "Ultimes 20 trucades que han accedit per radio",
+  "last_heard_list"             =>  "Últimes 20 trucades rebudes",
+  "local_tx_list"               =>  "Últimes 20 trucades rebudes via radio",
   "active_starnet_groups"       =>  "Grups actius Starnet",
   "active_starnet_members"      =>  "Membres actius grup Starnet",
   "d-star_link_manager"         =>  "Gestor d'enllaços D-Star",
-  "d-star_link_status"          =>  "Informacio d'enllaços D-Star",
+  "d-star_link_status"          =>  "Informació d'enllaços D-Star",
   "service_status"              =>  "Estat del servei"
 );
 ?>
