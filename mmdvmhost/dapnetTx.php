9b079aeb049e9c6d7e1a55a5facf4d90dd869d7f
Ben Horan <benh@geeksforhire.com.au>
2018-10-18 22:44:22 +1100

    Pi-Star Dashboard v20181020
    - FIXED:  Sidebar for Service, Network Status, etc - tidied up code logic for POCSAG related sections added in last commit
    
    - ADDED:  DAPNETGateway log parsing - extracts transmitted message info for display
    - ADDED:  New File "/mmdvmhost/dapnetTx.php" to display Dashboard table of POCSAG Transmitter activity
              (only visible if POCSAG mode is Enabled and DAPNETGateway Active)
    
    - TODO:   Add Internationalisation code to labels and headers of new information display. Ascertain whether there are contributors
              from different nationalities that can check the Language Pack file entries for each language is correct word/spelling
              (otherwise - Google Translate to build up base translations??)
    
    - TODO:   Add include to "/mmdvmhost/functions.php" to "/index.php" so that config items can be ascertained (place the Ajax script
              and PHP include for "dapnetTx.php" table in a conditional so that rather than "dapnetTx.php" simply returning no visible
              HTML if POCSAG isn't disabled, have it so that "index.php" doesn't reference it (or Ajax refresh it every 1500ms) at all!

---
+++ mmdvmhost/dapnetTx.php
@@ -0,0 +1,58 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
+include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
+include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';	      // Translation Code
+
+$isPocsagEnabled = ((getEnabled("POCSAG", $mmdvmconfigs) == 1) && (getEnabled("POCSAG Network", $mmdvmconfigs) == 1));
+$progname = basename($_SERVER['SCRIPT_FILENAME'],".php");
+$rev="20181020";
+$dapnetMessageLog = getDAPNETGatewayMessageLog();
+$reverseDapnetMessageLog = $dapnetMessageLog;
+array_multisort($reverseDapnetMessageLog, SORT_DESC);
+
+if ($isPocsagEnabled) {
+    //TODO: Ben Horan <benh@geeksforhire.com.au> 2018-10-20
+    //TODO: Add language internationalisation functionality and language packs rather than hardcoded English labels
+?>
+
+<b>POCSAG Message Transmission History</b>
+
+<table style="width: 644px;">
+  <tr>
+    <th><a class="tooltip" href="#"><?php echo $lang['time'];?> (<?php echo date('T')?>)<span><b>Time in <?php echo date('T')?> time zone</b></span></a></th>
+    <th><a class="tooltip" href="#">TimeSlot<span><b>Message Mode</b></span></a></th>
+    <th><a class="tooltip" href="#">Pager RIC<span><b>RIC / CapCode of the receiving Pager</b></span></a></th>
+    <th><a class="tooltip" href="#">Message<span><b>Message contents</b></span></a></th>
+    <th><a class="tooltip" href="#"><?php echo $lang['src'];?><span><b>Recieved from source</b></span></a></th>
+  </tr>
+
+<?php
+  foreach ($reverseDapnetMessageLog as $dapnetMessageEntry) {
+      $utc_time = $dapnetMessageEntry["Date"]." ".substr($dapnetMessageEntry["Time"],0,-4);
+      $utc_tz =  new DateTimeZone('UTC');
+      $local_tz = new DateTimeZone(date_default_timezone_get ());
+      $dt = new DateTime($utc_time, $utc_tz);
+      $dt->setTimeZone($local_tz);
+      $local_time = $dt->format('H:i:s M jS');
+      //DEBUG:
+      //echo '<!-- DEBUG:  $utc_time = ' . $utc_time . ', $local_time = ' . $local_time . '-->';
+?>
+
+  <tr>
+    <td style="width: 140px; vertical-align: top; text-align: center;"><?php echo $local_time ?></td>
+    <td style="width: 70px; vertical-align: top; text-align: center;"><?php echo "TS ".$dapnetMessageEntry["TxTimeSlot"] ?></td>
+    <td style="width: 90px; vertical-align: top; text-align: center;"><?php echo $dapnetMessageEntry["RIC"] ?></td>
+    <td style="width: max-content; vertical-align: top; text-align: center; word-wrap: break-word; white-space: normal !important;"><?php echo $dapnetMessageEntry["Message"] ?></td>
+    <td style="width: 60px; vertical-align: top; text-align: center;">DAPNET</td>
+  </tr>
+
+<?php
+  } // foreach
+?>
+
+</table>
+
+<?php
+} // if($isPocsagEnabled)
+?>
