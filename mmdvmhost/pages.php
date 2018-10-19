e126cedac5f34bd991769f38f4ad313a4405dac0
Andy Taylor <andy@always-online.co.uk>
2018-10-19 15:49:45 +0100

    Update pages.php

--- mmdvmhost/pages.php
+++ mmdvmhost/pages.php
@@ -4,7 +4,7 @@
 include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
-include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';	      // Translation Code
+include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';        // Translation Code
 ?>
 <b><?php echo $lang['dapnet_activity_hdr'];?></b>
 <table>
