bc83ebca7ceb9a17ee139e8ac3d5b2f35b54c07c
Andy Taylor <andy@always-online.co.uk>
2018-10-19 15:25:34 +0100

    Update pages.php

--- mmdvmhost/pages.php
+++ mmdvmhost/pages.php
@@ -35,7 +35,7 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';	      // Translat
     <td style="width: 140px; vertical-align: top; text-align: center;"><?php echo $local_time; ?></td>
     <td style="width: 70px; vertical-align: top; text-align: center;"><?php echo "TS ".$pocsag_timeslot; ?></td>
     <td style="width: 90px; vertical-align: top; text-align: center;"><?php echo $pocsag_ric; ?></td>
-    <td style="width: max-content; vertical-align: top; text-align: center; word-wrap: break-word; white-space: normal !important;"><?php echo $dapnetMessageEntry["Message"] ?></td>
+    <td style="width: max-content; vertical-align: top; text-align: center; word-wrap: break-word; white-space: normal !important;"><?php echo $pocsag_msg; ?></td>
     <td style="width: 60px; vertical-align: top; text-align: center;">DAPNET Gateway</td>
   </tr>
 
