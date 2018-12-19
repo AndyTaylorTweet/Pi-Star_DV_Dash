36582b4db810373465a8c6896318e32439859c4f
Andy Taylor <andy@always-online.co.uk>
2018-12-19 11:38:48 +0000

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -3396,7 +3396,7 @@ $p25Hosts = fopen("/usr/local/etc/P25Hosts.txt", "r");
         <th colspan="2"><a class="tooltip" href="#"><?php echo $lang['value'];?><span><b>Value</b>The current value from the<br />configuration files</span></a></th>
       </tr>
       <tr>
-        <td align="left"><a class="tooltip2" href="#">POCSAG Server:<span><b>POCSAG Server</b>Set the POCSAG Network<br /> here</span></a></td>
+        <td align="left"><a class="tooltip2" href="#">POCSAG Server:<span><b>POCSAG Server</b>Set the POCSAG Network here</span></a></td>
 	<td style="text-align: left;"><select name="pocsagServer">
         	<option value="<?php echo $configdapnetgw['DAPNET']['Address'];?>" selected="selected"><?php echo $configdapnetgw['DAPNET']['Address'];?></option>
 		<option value="dapnet.afu.rwth-aachen.de">dapnet.afu.rwth-aachen.de</option>
