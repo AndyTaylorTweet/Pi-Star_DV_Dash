bc67e30d765a33f7bc6cff0bcffab5507f5204f3
Andy Taylor <andy@always-online.co.uk>
2018-12-24 13:03:38 +0000

    Update config_backup.php

--- admin/config_backup.php
+++ admin/config_backup.php
@@ -254,7 +254,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/config_backup.php") {
   </div>
   <div class="footer">
   Pi-Star web config, &copy; Andy Taylor (MW0MWZ) 2014-<?php echo date("Y"); ?>.<br />
-  Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistar/" target="_new">here for the Support Group</a><br />
+  Need help? Click <a style="color: #ffffff;" href="https://www.facebook.com/groups/pistarusergroup/" target="_new">here for the Support Group</a><br />
   Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.pistar.uk/downloads/" target="_blank">here</a>.<br />
   <br />
   </div>
