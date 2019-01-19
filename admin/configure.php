7597ae6b4a87571761b697a7a3b32799c0b71153
Andy Taylor <andy@always-online.co.uk>
2019-01-19 11:11:49 +0000

    Update configure.php
    updated javascript

--- admin/configure.php
+++ admin/configure.php
@@ -201,7 +201,7 @@ $MYCALL=strtoupper($callsign);
 		document.getElementById("confLongitude").value = position.coords.longitude.toFixed(5);
 	}
     </script>
-    <script type="text/javascript" src="/functions.js?version=1.4"></script>
+    <script type="text/javascript" src="/functions.js?version=1.5"></script>
 </head>
 <body onload="checkFrequency(); return false;">
 <div class="container">
