0fc8fe8f6a9cd558614f238fc60b85f598a9dc51
Andy Taylor <andy@always-online.co.uk>
2019-01-27 19:01:18 +0000

    Create turkish_tr.php

---
+++ lang/turkish_tr.php
@@ -0,0 +1,145 @@
+<?php
+//
+// Turkish TR Language Pack
+// Translated by Cem Gurcan TB2TCG
+// Updated: 27-JAN-2019
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Dijital Ses",
+  "configuration"               =>  "Konfigürasyon",
+  "dashboard_for"               =>  "Panosu",
+  // Banner links
+  "dashboard"                   =>  "Pano",
+  "admin"                       =>  "Yönetici",
+  "power"                       =>  "Güç",
+  "update"                      =>  "Güncelle",
+  "backup_restore"              =>  "Yedekle/Geriyükle",
+  "factory_reset"               =>  "Fabrika Ayarları",
+  "live_logs"                   =>  "Sistem Günlüğü",
+  // Config page section headdings
+  "hardware_info"               =>  "Ağ Geçidi Donanım Bilgisi",
+  "control_software"            =>  "Kontrolcü Yazılımı",
+  "mmdvmhost_config"            =>  "MMDVMHost Konfigürasyonu",
+  "general_config"              =>  "Genel Konfigürasyon",
+  "dmr_config"                  =>  "DMR Konfigürasyonu",
+  "dstar_config"                =>  "D-Star Konfigürasyonu",
+  "ysf_config"                  =>  "Yaesu System Fusion Konfigürasyonu",
+  "p25_config"                  =>  "P25 Konfigürasyonu",
+  "nxdn_config"                 =>  "NXDN Konfigürasyonu",
+  "pocsag_config"               =>  "POCSAG Konfigürasyonu",
+  "wifi_config"                 =>  "Kablosuz Konfigürasyonu",
+  "fw_config"                   =>  "Güv.Duv. Konfigürasyonu",
+  "remote_access_pw"            =>  "Uzak Erişim Parolası",
+  // Config Page - Section General
+  "setting"                     =>  "Ayar",
+  "value"                       =>  "Değer",
+  "apply"                       =>  "Değişiklikleri Uygula",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Sistem Adı",
+  "kernel"                      =>  "Çekirdek Sürümü",
+  "platform"                    =>  "Altyapı",
+  "cpu_load"                    =>  "CPU Kullanımı",
+  "cpu_temp"                    =>  "CPU Isısı",
+  // Config Page - Control Software
+  "controller_software"         =>  "Kontrolcünün Yazılımı",
+  "controller_mode"             =>  "Kontrolcünün Modu",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Modu",
+  "d-star_mode"                 =>  "D-Star Modu",
+  "ysf_mode"                    =>  "YSF Modu",
+  "p25_mode"                    =>  "P25 Modu",
+  "nxdn_mode"                   =>  "NXDN Modu",
+  "mmdvm_display"               =>  "MMDVM Ekran Tipi",
+  "mode_hangtime"               =>  "Mod Askı Süresi",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Çağrı İşaretiniz",
+  "dmr_id"                      =>  "CCS7/DMR No",
+  "radio_freq"                  =>  "Radyo Frekansı",
+  "lattitude"                   =>  "Enlem",
+  "longitude"                   =>  "Boylam",
+  "town"                        =>  "Şehir",
+  "country"                     =>  "Ülke",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radyo/Modem Tipi",
+  "node_type"                   =>  "Yayın Tipi",
+  "timezone"                    =>  "Sistem Saati Dilimi",
+  "dash_lang"                   =>  "Pano Dili",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Sunucu (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Sunucu",
+  "bm_network"                  =>  "BrandMeister Ağı",
+  "dmr_plus_master"             =>  "DMR+ Sunucu",
+  "dmr_plus_network"            =>  "DMR+ Ağı",
+  "xlx_master"                  =>  "XLX Sunucu",
+  "xlx_enable"                  =>  "XLX Sunucu Etkinleştirme",
+  "dmr_cc"                      =>  "DMR Renk Kodu",
+  "dmr_embeddedlconly"          =>  "DMR GömülüLC",
+  "dmr_dumptadata"              =>  "DMR TAVeriDökümü",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 Çağrı İşareti",
+  "dstar_rpt2"                  =>  "RPT2 Çağrı İşareti",
+  "dstar_irc_password"          =>  "Uzak Erişim Parolası",
+  "dstar_default_ref"           =>  "Varsayılan Yansıtıcı",
+  "aprs_host"                   =>  "APRS Sunucusu",
+  "dstar_irc_lang"              =>  "ircDDBAğGeçidi Dili",
+  "dstar_irc_time"              =>  "Bildirimler",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF İşletme Ağı",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 İşletme Ağı",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN İşletme Ağı",
+  "nxdn_ran"                     =>  "NXDN RAN",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Pano Erişimi",
+  "fw_irc"                      =>  "ircDDBAğGeçidi Erişimi",
+  "fw_ssh"                      =>  "SSH Erişimi",
+  // Config Page - Password
+  "user"                        =>  "Kullanıcı Adı",
+  "password"                    =>  "Parola",
+  "set_password"                =>  "Parola Tekrar",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Etkinleştirilmiş Modlar",
+  "net_status"                  =>  "Ağ Durumu",
+  "internet"                    =>  "İnternet",
+  "radio_info"                  =>  "Radyo Bilgisi",
+  "dstar_repeater"              =>  "D-Star Röle",
+  "dstar_net"                   =>  "D-Star Ağı",
+  "dmr_repeater"                =>  "DMR Röle",
+  "dmr_master"                  =>  "DMR Sunucu",
+  "ysf_net"                     =>  "YSF Ağı",
+  "p25_radio"                   =>  "P25 Radyo",
+  "p25_net"                     =>  "P25 Ağı",
+  "nxdn_radio"                  =>  "NXDN Radyo",
+  "nxdn_net"                    =>  "NXDN Ağı",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Zaman",
+  "mode"                        =>  "Mod",
+  "callsign"                    =>  "Çağrı İşareti",
+  "target"                      =>  "Hedef",
+  "src"                         =>  "Kynk",        // Short version of "Source"
+  "dur"                         =>  "Süre",        // Short version of "Duration"
+  "loss"                        =>  "Kayıp",
+  "ber"                         =>  "BHO",        // Short version of "Bit Error Rate"
+  // POCSAG Specific
+  "pocsag_list"                 =>  "DAPNET Ağ Geçidi Etkinliği",
+  "pocsag_timeslot"             =>  "Zaman Aralığı",
+  "pocsag_msg"                  =>  "Mesaj",
+  // Dashboard - Extra Info
+  "group"                       =>  "Grup",
+  "logoff"                      =>  "Oturum Kapat",
+  "info"                        =>  "Ayrıntılar",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Ağ Geçidi Etkinliği",
+  "local_tx_list"               =>  "Yerel RF Etkinliği",
+  "active_starnet_groups"       =>  "Aktif Starnet Grupları",
+  "active_starnet_members"      =>  "Aktif Starnet Grup Üyeleri",
+  "d-star_link_manager"         =>  "D-Star Bağlantı Yöneticisi",
+  "d-star_link_status"          =>  "D-Star Bağlantı Ayrıntıları",
+  "service_status"              =>  "Servis Durumu"
+);
+?>
