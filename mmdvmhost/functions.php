d54b3fc5ef32d839536a601a7407f4ea7a6f1c1c
Spencer Fowler <spencer.fowler@is4s.com>
2019-02-01 16:07:57 +0000

    Rework LastHeard to prevent RF & Net traffic conflicts

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -678,8 +678,9 @@ function getLastHeard($logLines) {
 	$counter = 0;
 	foreach ($heardList as $listElem) {
 		if ( ($listElem[1] == "D-Star") || ($listElem[1] == "YSF") || ($listElem[1] == "P25") || ($listElem[1] == "NXDN") || ($listElem[1] == "POCSAG") || (startsWith($listElem[1], "DMR")) ) {
-			if(!(array_search($listElem[2]."#".$listElem[1].$listElem[3], $heardCalls) > -1)) {
-				array_push($heardCalls, $listElem[2]."#".$listElem[1].$listElem[3]);
+			$callUuid = $listElem[2]."#".$listElem[1].$listElem[3].$listElem[5];
+			if(!(array_search($callUuid, $heardCalls) > -1)) {
+				array_push($heardCalls, $callUuid);
 				array_push($lastHeard, $listElem);
 				$counter++;
 			}/*
