50be37d54b09be5b126ed31dbc57ec5386719296
Daniel Caujolle-Bert <f1rmb.daniel@gmail.com>
2019-02-23 17:29:42 +0100

    Reduce message maxlength ('x/n ' was forgotten). Add input field for transmitter group(s) setting. This setting is temporary until message is sent.

--- mmdvmhost/dapnet_messenger.php
+++ mmdvmhost/dapnet_messenger.php
@@ -21,10 +21,10 @@ if (! isset($configdapnetapi)) {
 
 if (isset($configdapnetapi['DAPNETAPI']['USER']) && (empty($configdapnetapi['DAPNETAPI']['USER']) != TRUE)):
 
-    $maxlength = (5 * (80 - (strlen($configdapnetapi['DAPNETAPI']['USER']) + 2 /* 'CALLSIGN: ' prefix */)));
+    $maxlength = (5 * (80 - (strlen($configdapnetapi['DAPNETAPI']['USER']) + 2 /* 'CALLSIGN: ' prefix */ + 4 /* 'x/n ' count */)));
     
     // Data has been posted for this page (POST)
-    if ((empty($_POST) != TRUE) && (isset($_POST["dapSubmit"]) && (empty($_POST["dapSubmit"]) != TRUE)) && (isset($_POST["dapToCallsign"]) && (empty($_POST["dapToCallsign"]) != TRUE)) && (isset($_POST["dapMsgContent"]) && (empty($_POST["dapMsgContent"]) != TRUE))) {
+    if ((empty($_POST) != TRUE) && (isset($_POST['dapSubmit']) && (empty($_POST['dapSubmit']) != TRUE)) && (isset($_POST['dapToCallsign']) && (empty($_POST['dapToCallsign']) != TRUE)) && (isset($_POST['dapMsgContent']) && (empty($_POST['dapMsgContent']) != TRUE))) {
         
         $dapnetTo = preg_replace('/[^,:space:[:alnum:]]/', "", trim($_POST['dapToCallsign'])); // Only A-Z a-z 0-9 and , allowed
         while (preg_match('/,,/', $dapnetTo)) { $dapnetTo = preg_replace('/,,/', ",", $dapnetTo); } // replace any double comma with single comma
@@ -33,11 +33,26 @@ if (isset($configdapnetapi['DAPNETAPI']['USER']) && (empty($configdapnetapi['DAP
         $filteredChars = array('\''=>'\\\'', '"'=>'\\\\\\"');
         $dapnetContent = strtr(str_replace(array("\r\n", "\n", "\r"), "", iconv('UTF-8','ASCII//TRANSLIT', $_POST['dapMsgContent'])), $filteredChars);
 
-        $dapnetCmd = 'sudo /usr/local/sbin/pistar-dapnetapi '.$dapnetTo.' "'.$dapnetContent.'" nohost 2>&1';
+        // TRX AREA
+        $dapnetTrx=((isset($_POST['dapToTrxArea']) && (empty($_POST['dapToTrxArea']) != TRUE)) ? preg_replace('/[^,:space:[:alnum:]-]/', "", trim(strtolower(trim($_POST['dapToTrxArea'])))) : "");
+        while (preg_match('/,,/', $dapnetTrx)) { $dapnetTrx = preg_replace('/,,/', ",", $dapnetTrx); } // Replace any double comma with single comma
+        while (preg_match('/--/', $dapnetTrx)) { $dapnetTrx = preg_replace('/--/', "-", $dapnetTrx); } // Replace any double dash with single dash
+        $dapnetTrx = rtrim($dapnetTrx, ","); // Remove comma at the end of the string, if any.
+
+        // if trx area is different that the one in api.key file, override it using DAPNET_TRXAREA envvar 
+        if (strlen($dapnetTrx) > 0 && (strcmp($dapnetTrx, $configdapnetapi['DAPNETAPI']['TRXAREA']) != 0)) {
+            $dapnetTrx = 'DAPNET_TRXAREA="'.$dapnetTrx.'"';
+        }
+        else {
+            $dapnetTrx = "";
+        }
+
+        // Build command line
+        $dapnetCmd = 'sudo '.$dapnetTrx.' /usr/local/sbin/pistar-dapnetapi '.$dapnetTo.' "'.$dapnetContent.'" nohost 2>&1';
         
         unset($dummy);
         
-        ## Send POCSAG Page
+        // Send POCSAG Page
         $resultapi = exec($dapnetCmd, $dummy, $retValue);
         
         // Output to the browser
@@ -52,21 +67,26 @@ if (isset($configdapnetapi['DAPNETAPI']['USER']) && (empty($configdapnetapi['DAP
 
     }
     else {
-    
+        $dapnetTrxAreas=(isset($configdapnetapi['DAPNETAPI']['TRXAREA']) && (! empty($configdapnetapi['DAPNETAPI']['TRXAREA'])) ? $configdapnetapi['DAPNETAPI']['TRXAREA'] : "");
+            
         echo '<b>DAPNET Messenger</b>'."\n";
         echo '<form action="'.htmlentities($_SERVER['PHP_SELF']).'" method="post">'."\n";
         echo '<table>
 		<tr>
-			<th><a class=tooltip href="#">To<span><b>Enter the destination callsign(s)</b>You can set many callsigns, comma separated</span></a></th>
+			<th><a class=tooltip href="#">To<span><b>Enter the destination callsign and Trx Area(s)</b>You can set many callsigns and transmitter areas, comma separated</span></a></th>
 			<th><a class=tooltip href="#">Message<span><b>Enter the message content</b>Length is limited to '.$maxlength.' characters (splitted in 5 POCSAG pages)</span></a></th>
 			<th><a class=tooltip href="#">Action<span><b>Send the message</b></span></a></th>
 		</tr>'."\n";
         echo '  <tr>';
-        echo '    <td><input type="text" name="dapToCallsign" size="13" maxlength="70" value="" /></td>';
-        echo '    <td><textarea maxlength="'.$maxlength.'" name="dapMsgContent" cols="60" rows="3" style="overflow:scroll;" value="" /></textarea></td>';
-        echo '    <td><input type="submit" value="Send" name="dapSubmit" /></td>';
+        echo '    <td><input type="text" name="dapToCallsign" size="10" maxlength="70" value="" /></td>';
+        echo '    <td rowspan="2"><textarea maxlength="'.$maxlength.'" name="dapMsgContent" cols="55" rows="3" style="overflow:scroll;" value="" /></textarea></td>';
+        echo '    <td rowspan="2" style="vertical-align:bottom;padding:5px;"><input type="submit" value="Send" name="dapSubmit" /></td>';
         echo '  </tr>'."\n";
+        echo '  <tr>';
+        echo '    <td><input type="text" name="dapToTrxArea" size="5" maxlength="50" value="'.$dapnetTrxAreas.'" /></tr>';
+        echo '  </tr>';
         echo '</table>'."\n";
+
 
     }
 
