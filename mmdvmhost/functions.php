505d2312273262da318651fae540f5d4a573d7d6
Andy Taylor <andy@always-online.co.uk>
2019-04-29 19:03:46 +0100

    Update functions.php

--- mmdvmhost/functions.php
+++ mmdvmhost/functions.php
@@ -305,7 +305,7 @@ function getDAPNETGatewayLog() {
         }
 	$logLines1 = array_filter($logLines1);
         if (sizeof($logLines1) == 0) {
-                if (file_exists("/var/log/pi-starDAPNETGateway-".gmdate("Y-m-d", time() - 86340).".log")) {
+                if (file_exists("/var/log/pi-star/DAPNETGateway-".gmdate("Y-m-d", time() - 86340).".log")) {
 			$logPath2 = "/var/log/pi-star/DAPNETGateway-".gmdate("Y-m-d", time() - 86340).".log";
 			$logLines2 = preg_split('/\r\n|\r|\n/', `egrep -h "Sending message" $logPath2 | cut -d" " -f2- | tail -n 20 | tac`);
                 }
