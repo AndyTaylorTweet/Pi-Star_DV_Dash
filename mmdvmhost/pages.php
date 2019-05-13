03192affca5878c3cbff4d5c051de0f4e8246aa0
Andy Taylor <andy@always-online.co.uk>
2019-05-13 17:20:54 +0100

    Update pages.php
    improved skyper decode some more

--- mmdvmhost/pages.php
+++ mmdvmhost/pages.php
@@ -1,20 +1,21 @@
 <?php
 // Most of the work here contributed by geeks4hire (Ben Horan)
+// Skyper decode by Andy Taylor (MW0MWZ)
 
 include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
 include_once $_SERVER['DOCUMENT_ROOT'].'/config/language.php';        // Translation Code
 
-// Function to reverse the ROT1 used for Skyper RIC 0004520
-function un_skyper($s, $n = 25) {
-        static $letters = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-        $n = (int)$n % 26;
-        if (!$n) return $s;
-        if ($n < 0) $n += 26;
-        $rep = substr($letters, $n * 2) . substr($letters, 0, $n * 2);
-        str_replace('!', ' ', $s);
-        return strtr($s, $letters, $rep);
+// Function to reverse the ROT1 used for Skyper
+function un_skyper($s, $n = -1) {
+  static $letters = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+  $n = (int)$n % 95;
+  if (!$n) return $s;
+  if ($n < 0) $n += 95;
+  $rep = substr($letters, $n * 1) . substr($letters, 0, $n * 1);
+  str_replace('!', ' ', $s);
+  return strtr($s, $letters, $rep);
 }
 ?>
 <b><?php echo $lang['pocsag_list'];?></b>
@@ -39,6 +40,12 @@ function un_skyper($s, $n = 25) {
       $pocsag_timeslot = $dapnetMessageArr["6"];
       $pocsag_ric = str_replace(',', '', $dapnetMessageArr["8"]);
       $pocsag_msg = $dapnetMessageTxtArr["1"];
+    
+      // Decode Skyper Messages
+      if ($pocsag_ric == "0004520") {
+        $pocsag_msg = un_skyper($pocsag_msg);
+      } 
+   
       // Formatting long messages without spaces
       if (strpos($pocsag_msg, ' ') == 0 && strlen($pocsag_msg) >= 45) {
         $pocsag_msg = wordwrap($pocsag_msg, 45, ' ', true);
@@ -49,7 +56,7 @@ function un_skyper($s, $n = 25) {
     <td style="width: 140px; vertical-align: top; text-align: left;"><?php echo $local_time; ?></td>
     <td style="width: 70px; vertical-align: top; text-align: center;"><?php echo "Slot ".$pocsag_timeslot; ?></td>
     <td style="width: 90px; vertical-align: top; text-align: center;"><?php echo $pocsag_ric; ?></td>
-    <td style="width: max-content; vertical-align: top; text-align: left; word-wrap: break-word; white-space: normal !important;"><?php if ($pocsag_ric == "0004520") { echo un_skyper($pocsag_msg); } else { echo $pocsag_msg; } ?></td>
+    <td style="width: max-content; vertical-align: top; text-align: left; word-wrap: break-word; white-space: normal !important;"><?php echo $pocsag_msg; ?></td>
   </tr>
 
 <?php
