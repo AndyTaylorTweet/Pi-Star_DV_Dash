1c237cb9249d5df5836d3d421df5c4f973a22356
b4d <b4d@sablun.org>
2019-06-17 21:14:48 +0200

    Init Slovenian translation

---
+++ lang/slovenian_sl.php
@@ -0,0 +1,145 @@
+<?php
+//
+// English UK Language Pack
+// Andy Taylor (MW0MWZ)
+// Updated: 02-Aug-2017
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Digital Voice",
+  "configuration"               =>  "Configuration",
+  "dashboard_for"               =>  "Dashboard for",
+  // Banner links
+  "dashboard"                   =>  "Dashboard",
+  "admin"                       =>  "Admin",
+  "power"                       =>  "Power",
+  "update"                      =>  "Update",
+  "backup_restore"              =>  "Backup/Restore",
+  "factory_reset"               =>  "Factory Reset",
+  "live_logs"                   =>  "Live Logs",
+  // Config page section headdings
+  "hardware_info"               =>  "Gateway Hardware Information",
+  "control_software"            =>  "Control Software",
+  "mmdvmhost_config"            =>  "MMDVMHost Configuration",
+  "general_config"              =>  "General Configuration",
+  "dmr_config"                  =>  "DMR Configuration",
+  "dstar_config"                =>  "D-Star Configuration",
+  "ysf_config"                  =>  "Yaesu System Fusion Configuration",
+  "p25_config"                  =>  "P25 Configuration",
+  "nxdn_config"                 =>  "NXDN Configuration",
+  "pocsag_config"               =>  "POCSAG Configuration",
+  "wifi_config"                 =>  "Wireless Configuration",
+  "fw_config"                   =>  "Firewall Configuration",
+  "remote_access_pw"            =>  "Remote Access Password",
+  // Config Page - Section General
+  "setting"                     =>  "Setting",
+  "value"                       =>  "Value",
+  "apply"                       =>  "Apply Changes",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Hostname",
+  "kernel"                      =>  "Kernel",
+  "platform"                    =>  "Platform",
+  "cpu_load"                    =>  "CPU Load",
+  "cpu_temp"                    =>  "CPU Temp",
+  // Config Page - Control Software
+  "controller_software"         =>  "Controller Software",
+  "controller_mode"             =>  "Controller Mode",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Mode",
+  "d-star_mode"                 =>  "D-Star Mode",
+  "ysf_mode"                    =>  "YSF Mode",
+  "p25_mode"                    =>  "P25 Mode",
+  "nxdn_mode"                   =>  "NXDN Mode",
+  "mmdvm_display"               =>  "MMDVM Display Type",
+  "mode_hangtime"               =>  "Mode Hangtime",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Node Callsign",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Radio Frequency",
+  "lattitude"                   =>  "Latitude",
+  "longitude"                   =>  "Longitude",
+  "town"                        =>  "Town",
+  "country"                     =>  "Country",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Radio/Modem Type",
+  "node_type"                   =>  "Node Type",
+  "timezone"                    =>  "System Time Zone",
+  "dash_lang"                   =>  "Dashboard Language",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Master (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Master",
+  "bm_network"                  =>  "BrandMeister Network",
+  "dmr_plus_master"             =>  "DMR+ Master",
+  "dmr_plus_network"            =>  "DMR+ Network",
+  "xlx_master"                  =>  "XLX Master",
+  "xlx_enable"                  =>  "XLX Master Enable",
+  "dmr_cc"                      =>  "DMR Colour Code",
+  "dmr_embeddedlconly"          =>  "DMR EmbeddedLCOnly",
+  "dmr_dumptadata"              =>  "DMR DumpTAData",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "RPT1 Callsign",
+  "dstar_rpt2"                  =>  "RPT2 Callsign",
+  "dstar_irc_password"          =>  "Remote Password",
+  "dstar_default_ref"           =>  "Default Reflector",
+  "aprs_host"                   =>  "APRS Host",
+  "dstar_irc_lang"              =>  "ircDDBGateway Language",
+  "dstar_irc_time"              =>  "Time Announcements",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Startup Host",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Startup Host",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN Startup Host",
+  "nxdn_ran"                     =>  "NXDN RAN",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Dashboard Access",
+  "fw_irc"                      =>  "ircDDBGateway Remote",
+  "fw_ssh"                      =>  "SSH Access",
+  // Config Page - Password
+  "user"                        =>  "User Name",
+  "password"                    =>  "Password",
+  "set_password"                =>  "Set Password",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Modes Enabled",
+  "net_status"                  =>  "Network Status",
+  "internet"                    =>  "Internet",
+  "radio_info"                  =>  "Radio Info",
+  "dstar_repeater"              =>  "D-Star Repeater",
+  "dstar_net"                   =>  "D-Star Network",
+  "dmr_repeater"                =>  "DMR Repeater",
+  "dmr_master"                  =>  "DMR Master",
+  "ysf_net"                     =>  "YSF Network",
+  "p25_radio"                   =>  "P25 Radio",
+  "p25_net"                     =>  "P25 Network",
+  "nxdn_radio"                  =>  "NXDN Radio",
+  "nxdn_net"                    =>  "NXDN Network",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Time",
+  "mode"                        =>  "Mode",
+  "callsign"                    =>  "Callsign",
+  "target"                      =>  "Target",
+  "src"                         =>  "Src",        // Short version of "Source"
+  "dur"                         =>  "Dur",        // Short version of "Duration"
+  "loss"                        =>  "Loss",
+  "ber"                         =>  "BER",        // Short version of "Bit Error Rate"
+  // POCSAG Specific
+  "pocsag_list"                 =>  "DAPNET Gateway Activity",
+  "pocsag_timeslot"             =>  "Time Slot",
+  "pocsag_msg"                  =>  "Message",
+  // Dashboard - Extra Info
+  "group"                       =>  "Group",
+  "logoff"                      =>  "LogOff",
+  "info"                        =>  "Information",
+  "utot"                        =>  "UTOT",       // Short for User Timeout
+  "gtot"                        =>  "GTOT",       // Short for Group Timeout
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Gateway Activity",
+  "local_tx_list"               =>  "Local RF Activity",
+  "active_starnet_groups"       =>  "Active Starnet Groups",
+  "active_starnet_members"      =>  "Active Starnet Group Members",
+  "d-star_link_manager"         =>  "D-Star Link Manager",
+  "d-star_link_status"          =>  "D-Star Link Information",
+  "service_status"              =>  "Service Status"
+);
+?>
