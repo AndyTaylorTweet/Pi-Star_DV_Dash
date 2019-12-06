087702cf205a8395ebd7184fb6a485994bd3e228
Andy Taylor <andy@always-online.co.uk>
2019-12-06 17:30:52 +0000

    Update wifi.php

--- admin/wifi.php
+++ admin/wifi.php
@@ -237,6 +237,7 @@ echo '<br />
 <input type="button" value="WiFi Info" name="wlan0_info" onclick="document.location=\'?page=\'+this.name" /><br />
 <input type="hidden" id="Networks" name="Networks" />
 <div class="network" id="networkbox">'."\n";
+		if (!isset($wifiCountry)) { $wifiCountry = "JP" }
 		$output .= 'WiFi Regulatory Domain (Country Code) : <select name="wifiCountryCode">
 <option value="'.$wifiCountry.'" selected>'.$wifiCountry.'</option>'."\n";
 		exec('grep "^[^#]" /usr/share/zoneinfo/zone.tab | awk \'{print $1" - "$3}\'', $regDomains);
