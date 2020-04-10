a3f9d19100bf264e512e7b9cee8f4dd61d87b967
Matt <matthew.ayo@gmail.com>
2020-04-10 16:38:42 -0700

    1) Fixed bug in WiFi Configuration changes not triggering a restart of the wireless interface after changes. 2) Fixed bug in SSID strings containing ' or special characters from being written out to wpa_supplicant.conf correctly.

--- admin/wifi.php
+++ admin/wifi.php
@@ -281,18 +281,18 @@ echo '<br />
 			elseif ($ssid && !$psk) { $config .= "network={\n\tssid=\"$ssid\"\n\t#psk=\"\"\n\tkey_mgmt=NONE\n\tid_str=\"$x\"\n\tpriority=$priority\n}\n\n"; }
 			elseif ($ssid && $psk) {
 				$pskSalted = hash_pbkdf2("sha1",$psk, $ssid, 4096, 64);
-				$ssidHex = exec('python -c \'print("'.$ssid.'".encode("hex"));\'');
+				$ssidHex = bin2hex("$ssid");
 				$config .= "network={\n\t#ssid=\"$ssid\"\n\tssid=$ssidHex\n\t#psk=\"$psk\"\n\tpsk=$pskSalted\n\tid_str=\"$x\"\n\tpriority=$priority\n}\n\n";
-			}
+		}
 		}
 		file_put_contents('/tmp/wifidata', $config);
 		system('sudo mount -o remount,rw / && sudo cp -f /tmp/wifidata /etc/wpa_supplicant/wpa_supplicant.conf && sudo mount -o remount,ro /');
 		echo "Wifi Settings Updated Successfully\n";
 		// If Auto AP is on, dont restart the WiFi Card
 		if (!file_exists('/sys/class/net/wlan0_ap')) {
-			exec('sudo ifdown wlan0 && sleep 3 && sudo ifup wlan0');
+			exec('sudo ip link set wlan0 down && sleep 3 && sudo ip link set wlan0 up');
 		}
-		echo "<script>window.location.reload();</script>";
+		echo "<script>document.location='?page=\wlan0_info'</script>";
 
 	} elseif(isset($_POST['Scan'])) {
 		$return = '';
