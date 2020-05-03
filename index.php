58b7172667033173d2386dfab7fa85d84b70793b
Andy Taylor <andy@always-online.co.uk>
2020-05-03 14:16:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -161,6 +161,27 @@ if (file_exists('/etc/dstar-radio.mmdvmhost')) {
 	if ($_SERVER["PHP_SELF"] == "/admin/index.php") {               // Admin Only Options
                 include 'mmdvmhost/bm_manager.php';                     // DMR Link Manager
         }
+	$testMMDVModeYSFnet = getConfigItem("System Fusion Network", "Enable", $mmdvmconfigs);
+        if ( $testMMDVModeYSFnet == 1 ) {				// If YSF network is enabled, add these extra features.
+		if ($_SERVER["PHP_SELF"] == "/admin/index.php") { 	// Admin Only Option
+			echo '<script type="text/javascript">'."\n";
+			echo 'function reloadysfConnections(){'."\n";
+			echo '  $("#ysfConnects").load("/mmdvmhost/ysf_manager.php",function(){ setTimeout(reloadysfConnections,5000) });'."\n";
+			echo '}'."\n";
+			echo 'setTimeout(reloadysfConnections,5000);'."\n";
+			echo '$(window).trigger(\'resize\');'."\n";
+			echo '</script>'."\n";
+			echo '<div id="ysfConnects">'."\n";
+			include 'mmdvmhost/ysf_manager.php';		// YSF Links
+			echo '</div>'."\n";
+		}
+		if ($_SERVER["PHP_SELF"] == "/admin/index.php") {	// Admin Only Option
+			echo '<div id="ysfControl">'."\n";
+			include 'mmdvmhost/ysf_manager.php';
+			echo '</div>'."\n";
+			echo "<br />\n";
+		}
+	}
 	echo '<script type="text/javascript">'."\n";
 	echo 'function reloadLocalTx(){'."\n";
 	echo '  $("#localTxs").load("/mmdvmhost/localtx.php",function(){ setTimeout(reloadLocalTx,1500) });'."\n";
