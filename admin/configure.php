012a3557f8458360a60eb4f4f448081d333a2595
Andy Taylor <andy@always-online.co.uk>
2020-05-18 16:19:29 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -2222,7 +2222,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 		}
 		if (empty($_POST['mobilegps_port']) != TRUE ) {
 			$newMobileGPSport = preg_replace('/[^a-z0-9]/i', '', $_POST['mobilegps_port']);
-			system('sudo sed -i "/Port=/dev/c\\Port=/dev/'.$newMobileGPSport.'" /etc/mobilegps');
+			system('sudo sed -i "/Port=\/dev/c\\Port=/dev/'.$newMobileGPSport.'" /etc/mobilegps');
 		}
 		if (empty($_POST['mobilegps_speed']) != TRUE ) {
 			$newMobileGPSspeed = preg_replace('/[^0-9]/', '', $_POST['mobilegps_speed']);
