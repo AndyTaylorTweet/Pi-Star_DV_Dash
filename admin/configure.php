397b1535c19884ecbe28f3d4aa10762dca4d2164
Juan Dominguez <juan.dominguez.developer@gmail.com>
2020-07-10 10:17:34 -0700

    Set mmdvmhsdualbandgpio slot to 0 and DMR Network - duplex to 0

--- admin/configure.php
+++ admin/configure.php
@@ -1538,8 +1538,9 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	    $rollRepeaterType1 = 'sudo sed -i "/repeaterType1=/c\\repeaterType1=0" /etc/ircddbgateway';
 	    system($rollModemType);
 	    system($rollRepeaterType1);
-	    $configmmdvm['Modem']['Port'] = "/dev/ttyAMA0";
-	    $configmmdvm['General']['Duplex'] = 1;
+		$configmmdvm['Modem']['Port'] = "/dev/ttyAMA0";
+	    $configmmdvm['General']['Duplex'] = 0;
+	    $configmmdvm['DMR Network']['Slot1'] = 0;
 	  }
 
 	  if ( $confHardware == 'mmdvmhsdualhatgpio' ) {
