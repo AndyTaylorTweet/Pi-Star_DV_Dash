a996d910fd614c3e9b875727f0cfdaa630fe0280
Andy Taylor <andy@always-online.co.uk>
2020-08-20 18:31:06 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -236,7 +236,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
     <tr>
     <td><?php echo php_uname('n');?></td>
     <td><?php echo php_uname('r');?></td>
-    <td colspan="2"><?php echo exec('platformDetect.sh');?></td>
+    <td colspan="2"><?php echo exec('/usr/local/bin/platformDetect.sh');?></td>
     <td><?php echo $cpuLoad[0];?> / <?php echo $cpuLoad[1];?> / <?php echo $cpuLoad[2];?></td>
     <?php echo $cpuTempHTML; ?>
     </tr>
