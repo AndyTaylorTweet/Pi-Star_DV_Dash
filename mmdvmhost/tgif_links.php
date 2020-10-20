ccafbaebf150d4988283205371f29107a6d02e91
Andy Taylor <andy@always-online.co.uk>
2020-10-20 10:03:21 +0100

    Update tgif_links.php

--- mmdvmhost/tgif_links.php
+++ mmdvmhost/tgif_links.php
@@ -57,14 +57,14 @@ if ( $testMMDVModeDMR == 1 ) {
   }
 
   // Use TGIF API to get information about current TGs
-   $jsonContext = stream_context_create(array('http'=>array('timeout' => 2, 'header' => 'User-Agent: Pi-Star Dashboard for '.$dmrID) )); // Add Timout and User Agent to include DMRID
+  $jsonContext = stream_context_create(array('http'=>array('timeout' => 2, 'header' => 'User-Agent: Pi-Star Dashboard for '.$dmrID) )); // Add Timout and User Agent to include DMRID
   $json_data = file_get_contents("http://tgif.network:5040/api/sessions", false, $jsonContext);
   $json = json_decode($json_data, false);
   // Pull the information from JSON
-  if ($json->sessions[1]->repeater_id == $dmrID) {
-    $repeaterid = $json->sessions[1]->repeater_id;
-    if ($json->sessions[1]->tg0 == "4000") { $slot1tg = "None"; } else { $slot1tg = "TG".$json->sessions[1]->tg0; }
-    if ($json->sessions[1]->tg == "4000") { $slot2tg = "None"; } else { $slot2tg = "TG".$json->sessions[1]->tg; }
+  if ($json->sessions[0]->repeater_id == $dmrID) {
+    $repeaterid = $json->sessions[0]->repeater_id;
+    if ($json->sessions[0]->tg0 == "4000") { $slot1tg = "None"; } else { $slot1tg = "TG".$json->sessions[0]->tg0; }
+    if ($json->sessions[0]->tg == "4000") { $slot2tg = "None"; } else { $slot2tg = "TG".$json->sessions[0]->tg; }
     
     echo '<b>Active TGIF Connections</b>
     <table>
