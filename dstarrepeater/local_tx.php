d1e0b95f241c81f0f83465773668a5ed27c6e09d
Andy Taylor <andy@always-online.co.uk>
2021-01-25 18:23:07 +0000

    Update local_tx.php

--- dstarrepeater/local_tx.php
+++ dstarrepeater/local_tx.php
@@ -52,8 +52,8 @@ $MYCALL=strtoupper($callsign);
                     $dt->setTimeZone($local_tz);
                     $local_time = $dt->format('H:i:s M jS');
                 print "<td align=\"left\">$local_time</td>";
-		print "<td align=\"left\" width=\"180\"><a href=\"http://www.qrz.com/db/$MyCallLink\" target=\"_blank\">$MyCall</a>";
-		//print "<td align=\"left\" width=\"180\"><a href=\"https://database.radioid.net/database/view?callsign=$MyCallLink\" target=\"popup\" onclick=\"window.open('https://database.radioid.net/database/view?callsign=$MyCallLink','popup','width=600,height=600'); return false;\">$MyCall</a>";
+		//print "<td align=\"left\" width=\"180\"><a href=\"http://www.qrz.com/db/$MyCallLink\" target=\"_blank\">$MyCall</a>";
+		print "<td align=\"left\" width=\"180\"><a href=\"https://database.radioid.net/database/view?callsign=$MyCallLink\" target=\"popup\" onclick=\"window.open('https://database.radioid.net/database/view?callsign=$MyCallLink','popup','width=600,height=600'); return false;\">$MyCall</a>";
                 if($MyId) { print "/".$MyId."</td>"; } else { print "</td>"; }
                 print "<td align=\"left\" width=\"100\">$YourCall</td>";
                 print "<td align=\"left\" width=\"100\">$Rpt1</td>";
