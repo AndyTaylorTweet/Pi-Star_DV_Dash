2fb32a79b606d4e94c7caa9e2df7f6a96c1e891c
Andy Taylor <andy@always-online.co.uk>
2021-01-27 12:18:11 +0000

    Update edit_dashboard.php

--- admin/expert/edit_dashboard.php
+++ admin/expert/edit_dashboard.php
@@ -156,7 +156,7 @@ echo '<form action="" method="post">'."\n";
 		echo "<br /><br />\n";
 	}
 echo "</form>";
-echo "<br /><br />\n";
+echo "<br />\n";
 echo 'if you took it all too far and now it makes you feel sick, click below to reset the changes made on this page.'."\n";
 echo '<form id="factoryReset" action="" method="post">'."\n";
 echo '  <div><input type="hidden" name="factoryReset" value="1" /></div>'."\n";
