87f15953c729c0083e0c359eeb8c58ee23ae3d94
Andy Taylor <andy@always-online.co.uk>
2021-01-27 12:06:15 +0000

    Update gateway_software_config.php

--- dstarrepeater/gateway_software_config.php
+++ dstarrepeater/gateway_software_config.php
@@ -1,7 +1,7 @@
 <table>
 <tr><th>ircDDB Network</th><th>APRS Host</th><th>CCS</th><th>DCS</th><th>DExtra</th><th>DPlus</th><th>D-Rats</th><th>Info</th><th>ircDDB</th><th>Echo</th><th>Log</th></tr>
 <tr style="background: #ffffff;">
-  <td><?php print $configs[ircddbHostname]; ?></td>
+  <td><?php print $configs['ircddbHostname']; ?></td>
   <td><?php if($configs['aprsEnabled'] == 1){ print $configs['aprsHostname']; } else { print "<img src=\"images/20red.png\">";} ?></td>
   <?php
   if($configs['ccsEnabled'] == 1){print "<td style=\"background-color:#1d1;\">ON</td>"; } else { print "<td style=\"background:#606060; color:#b0b0b0;\">OFF</td>"; }
