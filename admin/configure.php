05ad595a52d0975f7252cdee72091628e598f2bd
Andy Taylor <andy@always-online.co.uk>
2021-02-12 18:51:58 +0000

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -3492,9 +3492,9 @@ else:
     <input type="radio" name="nodeMode" value="pub"<?php if ($configmmdvm['DMR']['SelfOnly'] == 0) {echo ' checked="checked"';} ?> />Public</td>
     </tr>
     <tr>
-    <td align="left"><a class="tooltip2" href="#"><?php echo $lang['aprs_host'];?>:<span><b>APRS Host</b>Set your prefered APRS host here</span></a></td>
+    <td align="left"><a class="tooltip2" href="#"><?php echo $lang['aprs_host'];?>:<span><b>APRS Host</b>Set your prefered APRS host here, enabling this feature will make your location public.</span></a></td>
 <?php if (file_exists('/etc/aprsgateway')) {
-    echo "<td align=\"left\">\n";
+    echo "<td align=\"left\" width=\"60\">\n";
 	if ( $configaprsgw['Enabled']['Enabled'] == 1 ) {
 		echo "<div class=\"switch\"><input id=\"toggle-aprsgateway\" class=\"toggle toggle-round-flat\" type=\"checkbox\" name=\"APRSGatewayEnable\" value=\"ON\" checked=\"checked\" aria-hidden=\"true\" tabindex=\"-1\" ".$toggleAPRSGatewayCheckboxCr." /><label id=\"aria-toggle-aprsgateway\" role=\"checkbox\" tabindex=\"0\" aria-label=\"Enable APRS Position Reporting\" aria-checked=\"true\" onKeyPress=\"toggleAPRSGatewayCheckbox()\" onclick=\"toggleAPRSGatewayCheckbox()\" for=\"toggle-aprsgateway\"><font style=\"font-size:0px\">Enable APRS Position Reporting</font></label></div>";
 		}
