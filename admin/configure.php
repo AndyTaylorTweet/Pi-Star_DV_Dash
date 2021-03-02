4b55e25974a3533e3387a782c6708c4e57a52bdb
Andy Taylor <andy@always-online.co.uk>
2021-03-02 18:12:05 +0000

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -1164,7 +1164,7 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 		}
 
 		// Set the DMR+ / HBLink Options= line
-		if ((substr($dmrMasterHostArr[3], 0, 4) == "DMR+") || (substr($dmrMasterHostArr[3], 0, 3) == "HB_")) {
+		if ((substr($dmrMasterHostArr[3], 0, 4) == "DMR+") || (substr($dmrMasterHostArr[3], 0, 3) == "HB_") || (substr($dmrMasterHostArr[3], 0, 8) == "FreeDMR_")) {
 			unset ($configmmdvm['DMR Network']['Local']);
 			unset ($configysf2dmr['DMR Network']['Local']);
 			if (empty($_POST['dmrNetworkOptions']) != TRUE ) {
