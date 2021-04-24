b8a9d652f5312f51d77d71d108684875345cf4a6
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2021-04-24 11:25:19 +0100

    Update sysinfo.php

--- admin/sysinfo.php
+++ admin/sysinfo.php
@@ -92,7 +92,7 @@ function formatSize( $bytes ) {
     <title>Pi-Star - <?php echo $lang['digital_voice']." ".$lang['dashboard']." - ".$lang['update'];?></title>
     <link rel="stylesheet" type="text/css" href="css/pistar-css.php" />
     <script type="text/javascript" src="/jquery.min.js"></script>
-    <script type="text/javascript" src="//creativecouple.github.com/jquery-timing/jquery-timing.min.js"></script>
+    <script type="text/javascript" src="/jquery-timing.min.js"></script>
     <style>  
     .progress .bar + .bar {
       -webkit-box-shadow: inset 1px 0 0 rgba(0, 0, 0, 0.15), inset 0 -1px 0 rgba(0, 0, 0, 0.15);
