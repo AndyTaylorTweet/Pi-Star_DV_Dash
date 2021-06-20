29b9ad86aeefc4982ede28fa83425013fa58eb33
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2021-06-20 13:39:18 +0100

    Update configure.php

--- admin/configure.php
+++ admin/configure.php
@@ -1918,8 +1918,8 @@ if ($_SERVER["PHP_SELF"] == "/admin/configure.php") {
 	  $uPNPoff = 'sudo sed -i \'/pistar-upnp.service/ s/^#*/#/\' /etc/crontab';
 	  $uPNPsvcOn = 'sudo systemctl enable pistar-upnp.timer';
 	  $uPNPsvcOff = 'sudo systemctl disable pistar-upnp.timer';
-	  $uPNPsvcStart = '(systemctl stop pistar-upnp.service && systemctl start pistar-upnp.service) &';
-	  $uPNPsvcStop = '(systemctl stop pistar-upnp.service) &';
+	  $uPNPsvcStart = '(sudo systemctl stop pistar-upnp.service && sudo systemctl start pistar-upnp.service) &';
+	  $uPNPsvcStop = '(sudo systemctl stop pistar-upnp.service) &';
 
 	  if (escapeshellcmd($_POST['uPNP']) == 'ON' )  { system($uPNPon); system($uPNPsvcOn); system($uPNPsvcStart); }
 	  if (escapeshellcmd($_POST['uPNP']) == 'OFF' ) { system($uPNPoff); system($uPNPsvcStop); system($uPNPsvcOff); }
