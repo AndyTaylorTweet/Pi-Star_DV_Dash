ad59231c42e5b989d3d6ec76b51fb5ca4f5f75ae
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2024-01-28 12:14:08 +0000

    Update modem_fw_upgrade.php

--- admin/expert/modem_fw_upgrade.php
+++ admin/expert/modem_fw_upgrade.php
@@ -147,9 +147,12 @@ if ($_SERVER["PHP_SELF"] == "/admin/expert/modem_fw_upgrade.php") {
 	<h2>Modem Firmware Upgrade Utility</h2>
 	<p>This tool will attempt to upgrade your selected modem to the latest version available firmware version:<br />
 	<?php echo $fw_ver_msg; ?>
-	<p>When ready, select your modem type below and click, "Upgrade Modem". Do not interrupt the process or navigate away from the page while the process is running.</p>
+	<p>When ready, select your modem type below and click, "Upgrade Modem". Do not interrupt the process or<br />
+	navigate away from the page while the process is running.</p>
 	<p><strong><i class="fa fa-exclamation-circle"></i> Please understand what you are doing, as well as the risks associated with flashing your modem.</strong></p>
-	<p><em>(IMPORTANT: Please note, we are not firmware developers, and we offer no support for firmware. We provide utilities to update the firmware. For firmware support, you will need to utilise other support resources from the firmware developers/maintainers or the web.)</em></p>
+	<p><em>(IMPORTANT: Please note, we are not firmware developers, and we offer no support for firmware.<br />
+	We provide utilities to update the firmware. For firmware support, you will need to utilise other<br />
+	support resources from the firmware developers/maintainers or the web.)</em></p>
   </td></tr>
   <tr><td>
 <?php
