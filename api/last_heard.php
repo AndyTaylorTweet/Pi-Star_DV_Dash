1f7be3e64996d7e84aafa28bb4a7ba4e58f3e7e7
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2025-11-21 12:04:49 +0000

    Add security headers to last_heard.php

--- api/last_heard.php
+++ api/last_heard.php
@@ -1,4 +1,7 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config/security_headers.php');
+setEmbeddableSecurityHeaders();
+
 include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDash Config
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
