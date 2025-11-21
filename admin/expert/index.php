8ed878add0041681f41c57d0a421d1d9400e9e76
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2025-11-21 12:13:42 +0000

    Refactor index.php to include security headers

--- admin/expert/index.php
+++ admin/expert/index.php
@@ -1,4 +1,7 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config/security_headers.php');
+setSecurityHeaders();
+
 // Load the language support
 require_once('../config/language.php');
 //Load the Pi-Star Release file
@@ -62,3 +65,4 @@ Get your copy of Pi-Star from <a style="color: #ffffff;" href="http://www.pistar
 </div>
 </body>
 </html>
+
