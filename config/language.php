d5fc08dd98efd76a835a9d4caf1789c5db3f3249
MW0MWZ <66065904+MW0MWZ@users.noreply.github.com>
2025-12-01 16:46:39 +0000

    Remove security headers from language.php
    Removed security headers inclusion from language configuration.

--- config/language.php
+++ config/language.php
@@ -1,7 +1,4 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/config/security_headers.php');
-setSecurityHeaders();
-
 // Set the language
 $pistarLanguage='english_uk';
 include_once $_SERVER['DOCUMENT_ROOT']."/lang/$pistarLanguage.php";
